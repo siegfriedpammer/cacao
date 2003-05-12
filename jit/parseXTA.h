62503f216116796d12c292f0f43f61390ee4e428
Carolyn Oates <devnull@localhost>
2003-05-12 20:21:47 +0000

    *** empty log message ***

---
+++ jit/parseXTA.h
@@ -0,0 +1,19 @@
+/********************** parseRT.h ******************************************
+  Parser and print functions for X Type Analyis (XTA)
+  used to only compile methods that may actually be used.
+
+  5/12/2003 XTA Stub only - CO
+***************************************************************************/
+
+
+static void parseXTA()
+{
+
+}
+
+/*-------------------------------------------------------------------------------*/
+
+void XTA_jit_parse(methodinfo *m)
+{
+/*XTAprint*/ printf("XTA is under construction\n");
+}
