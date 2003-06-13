94a67c094f314ecd19b616126c7ef1f5896d306e
Christian Thalinger <twisti@complang.tuwien.ac.at>
2003-06-13 14:51:45 +0000

    boehm ifdef's

--- i386/methodtable.c
+++ i386/methodtable.c
@@ -4,18 +4,30 @@ static mtentry *mtroot = NULL;
 
 void addmethod(u1 *start, u1 *end)
 {
+#ifdef USE_BOEHM
     mtentry *mte = GCNEW(mtentry, 1);
+#else
+    mtentry *mte = NEW(mtentry);
+#endif
 
 /*      printf("start=%lx end=%lx\n", start, end); */
 
     if (mtroot == NULL) {
-	mtentry *tmp = GCNEW(mtentry, 1);
+#ifdef USE_BOEHM
+        mtentry *tmp = GCNEW(mtentry, 1);
+#else
+        mtentry *tmp = NEW(mtentry);
+#endif
 	tmp->start = (u1 *) asm_calljavamethod;
 	tmp->end = (u1 *) asm_calljavafunction;    /* little hack, but should work */
 	tmp->next = mtroot;
 	mtroot = tmp;
 
-	tmp = GCNEW(mtentry, 1);
+#ifdef USE_BOEHM
+        tmp = GCNEW(mtentry, 1);
+#else
+        tmp = NEW(mtentry);
+#endif
 	tmp->start = (u1 *) asm_calljavafunction;
 	tmp->end = (u1 *) asm_call_jit_compiler;    /* little hack, but should work */
 	tmp->next = mtroot;
