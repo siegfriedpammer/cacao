1c9c34b2a9a2312112c17d903e887c2cba194b07
Christian Thalinger <twisti@complang.tuwien.ac.at>
2003-10-22 20:56:30 +0000

    Removed obsolete dis-stuff.h and moved #include's here.

--- x86_64/i386-dis.c
+++ x86_64/i386-dis.c
@@ -36,13 +36,13 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
  */
 
 #include "dis-asm.h"
-/*  #include "sysdep.h" */
-/*  #include "opintl.h" */
-#include "dis-stuff.h"
 
 #define MAXLEN 20
 
 #include <setjmp.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #ifndef UNIXWARE_COMPAT
 /* Set non-zero for broken, compatible instructions.  Set to zero for
