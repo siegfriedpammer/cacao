ec478cc47ac8407cacf9b519cafe0db7ef8cc906
Christian Thalinger <twisti@complang.tuwien.ac.at>
2003-11-13 15:07:16 +0000

    Added double mul and div test up to infinity.

--- tests/fp.java
+++ tests/fp.java
@@ -1,15 +1,24 @@
 public class fp {
     public static void main(String [] s) {
-        float a = 10, b = 10;
+        p("-------------------- testfloat --------------------");
+        testfloat(10.0F, 10.0F);
+
+        p("-------------------- testdouble --------------------");
+        testdouble(10.0, 10.0);
+    }
+
+    public static void testfloat(float a, float b) {
         int i;
-		
+        float x = a;
+
+        p("---------- test fmul ----------");
         for (i = 0; i < 50; i++) {
             a *= b;
             p(a);
         }
-		
-        a = 10;
 
+        p("---------- test fdiv ----------");
+        a = x;
         for (i = 0; i < 50; i++) {
             a /= b;
             p(a);
@@ -17,7 +26,7 @@ public class fp {
 		
         for (a = 0; a < 1; a += 0.2) {
             for (b = 0; b < 1; b += 0.2) {
-                System.out.println ("-----------");
+                System.out.println("-----------");
                 p(a);
                 p(b);
                 p(a + b);
@@ -28,11 +37,46 @@ public class fp {
         }
     }
 		
-    public static void p(double d) {
-        System.out.println (d);
+    public static void testdouble(double a, double b) {
+        int i;
+        double x = a;
+
+        p("---------- test dmul ----------");
+        for (i = 0; i < 330; i++) {
+            a *= b;
+            p(a);
+        }
+
+        p("---------- test ddiv ----------");
+        a = x;
+        for (i = 0; i < 330; i++) {
+            a /= b;
+            p(a);
+        }
+		
+        for (a = 0; a < 1; a += 0.2) {
+            for (b = 0; b < 1; b += 0.2) {
+                System.out.println("-----------");
+                p(a);
+                p(b);
+                p(a + b);
+                p(a - b);
+                p(a * b);
+                p(a / b);
+            }
+        }
     }
+		
+    public static void p(String s) {
+        System.out.println(s);
+    }
+
+    public static void p(double d) {
+        System.out.println(d);
+    }
+
     public static void p(float d) {
-        System.out.println (d);
+        System.out.println(d);
     }
 
 }
