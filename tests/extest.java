d6e2760d1fb60daded4a139e667162fe16146c70
Christian Thalinger <twisti@complang.tuwien.ac.at>
2004-01-06 16:37:19 +0000

    These tests should check all possible exceptions.

--- tests/extest.java
+++ tests/extest.java
@@ -1,18 +1,60 @@
-import java.io.*;
+public class ex {
+    public static void main(String[] argv) {
+    	try {
+            System.out.print("THROWS: ");
+    	    sub();
+            System.out.println("failed.");
+    	} catch (Exception e) {
+      	    System.out.println("passed.");
+    	}
 
-public class extest extends Object
-{
-    public static void main (String args[])
-    {
-        try
-        {
-            RandomAccessFile file = new RandomAccessFile("test.file", "rw");
-            
-            file.seek(file.length());
-        }
-        catch (Exception exc)
-        {
-            System.out.println("could not open file");
-        }
+  	try {
+            System.out.print("NATIVE: ");
+            System.arraycopy(null, 1, null, 1, 1);
+            System.out.println("failed.");
+    	} catch (Exception e) {
+  	    System.out.println("passed");
+  	}
+
+        try {
+            System.out.print("NULL: ");
+            int[] ia = null;
+            int i = ia.length;
+            System.out.println("failed.");
+        } catch (NullPointerException e) {
+  	    System.out.println("passed.");
+  	}
+
+        try {
+            System.out.print("ARITHMETIC: ");
+            int i = 1;
+            int j = 0;
+            int k = i / j;
+            System.out.println("failed.");
+        } catch (ArithmeticException e) {
+  	    System.out.println("passed.");
+  	}
+
+        System.out.println("NULL (without catch): ");
+        String s = null;
+        int i = s.length();
+        System.out.println("failed.");
+    }
+
+    public synchronized static void sub() throws Exception {
+        int a, b, c, d;
+	sub2();
+    }
+
+    public static void sub2() throws Exception {
+	sub3();
+    }
+
+    public synchronized static void sub3() throws Exception {
+	sub4();
+    }
+
+    public static void sub4() throws Exception {
+	throw new Exception();
     }
 }
