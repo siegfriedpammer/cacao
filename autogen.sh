f121bdc29bcc939daffb69bba1e5cf3fd0903e40
Christian Thalinger <twisti@complang.tuwien.ac.at>
2004-03-22 20:51:52 +0000

    Generate all autmake and autoconf files.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,10 @@
-#! /bin/sh
+#!/bin/sh
 
 aclocal
 autoheader
-automake --add-missing
+automake --add-missing --include-deps
 autoconf
+
+cd mm/boehm-gc && ./autogen.sh && cd ../..
+cd gnuclasspathnat && ./autogen.sh && cd ../..
+
