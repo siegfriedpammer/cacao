7472a018b39d84bbbc30075e8d045530ae63c065
Stefan Ring <stefan@complang.tuwien.ac.at>
2004-06-11 15:45:05 +0000

    Atomic operations for MIPS

---
+++ src/vm/jit/mips/machine-instr.h
@@ -0,0 +1,54 @@
+/* If anyone wants to compile with gcc, use this file.
+ * It is not usable with the MIPSPro compiler.
+ */ 
+
+#ifndef _MACHINE_INSTR_H
+#define _MACHINE_INSTR_H
+
+static inline long
+__attribute__ ((unused))
+atomic_swap (volatile long *p, long val)
+{
+  long ret, temp;
+
+  __asm__ __volatile__
+    ("1:\n\t"
+     ".set  push\n\t"
+     ".set  mips2\n\t"
+     "lld   %2,%4\n\t"
+     "move  %0,%3\n\t"
+     "scd   %0,%1\n\t"
+     ".set  pop\n\t"
+     "beqz  %0,1b\n"
+     : "=&r" (temp), "=m" (*p), "=&r" (ret)
+     : "r" (val), "m" (*p)
+     : "memory");
+
+  return ret;
+}
+static inline int
+__attribute__ ((unused))
+compare_and_swap (volatile long int *p, long int oldval, long int newval)
+{
+  long int ret, temp;
+
+  __asm__ __volatile__
+    ("1:\n\t"
+     ".set  push\n\t"
+     ".set  mips2\n\t"
+     "lld   %1,%5\n\t"
+     "move  %0,$0\n\t"
+     "bne   %1,%3,2f\n\t"
+     "move  %0,%4\n\t"
+     "scd   %0,%2\n\t"
+     ".set  pop\n\t"
+     "beqz  %0,1b\n"
+     "2:\n\t"
+     : "=&r" (ret), "=&r" (temp), "=m" (*p)
+     : "r" (oldval), "r" (newval), "m" (*p)
+     : "memory");
+
+  return ret;
+}
+
+#endif
