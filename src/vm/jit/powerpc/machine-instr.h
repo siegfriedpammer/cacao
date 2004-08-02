ccfe637982032725b1a676a8d863160e30ca2ce8
Stefan Ring <stefan@complang.tuwien.ac.at>
2004-08-02 07:04:34 +0000

    preparation for new threads

--- src/vm/jit/powerpc/machine-instr.h
+++ src/vm/jit/powerpc/machine-instr.h
@@ -1,32 +1,25 @@
 #ifndef _MACHINE_INSTR_H
 #define _MACHINE_INSTR_H
 
-static inline long
-atomic_swap (volatile long *p, long val)
+static inline long compare_and_swap(long *p, long oldval, long newval)
 {
-  long int result;
-  __asm__ __volatile__ ("\n\
-0:  lwarx   %0,0,%1 \n\
-    stwcx.  %2,0,%1 \n\
-    bne-    0b  \n\
-" : "=&r"(result) : "r"(p), "r"(val) : "cr0", "memory");
-  return result;
+    if (*p == oldval) {
+        *p = newval;
+        return oldval;
+    } else
+        return *p;
 }
 
-static inline int
-compare_and_swap (volatile long *p, long oldval, long newval)
+static inline void
+atomic_add(int *mem, int val)
 {
-  int result;
-  __asm__ __volatile__ ("\n\
-0:  lwarx   %0,0,%1 \n\
-    sub%I2c.  %0,%0,%2    \n\
-    cntlzw  %0,%0   \n\
-    bne-    1f  \n\
-    stwcx.  %3,0,%1 \n\
-    bne-    0b  \n\
-1:  \n\
-" : "=&b"(result) : "r"(p), "Ir"(oldval), "r"(newval) : "cr0", "memory");
-  return result >> 5;
+	*mem += val;
 }
 
+#define STORE_ORDER_BARRIER() __asm__ __volatile__ ("" : : : "memory");
+#define MEMORY_BARRIER_BEFORE_ATOMIC() __asm__ __volatile__ ("sync" : : : "memory");
+#define MEMORY_BARRIER_AFTER_ATOMIC() __asm__ __volatile__ ("" : : : "memory");
+#define MEMORY_BARRIER() __asm__ __volatile__ ( "sync" : : : "memory" );
+
+#endif
 #endif
