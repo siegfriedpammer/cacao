536551ff40d9ab1debc787ef7ae15ae8b324ae3d
Stefan Ring <stefan@complang.tuwien.ac.at>
2004-11-17 12:06:32 +0000

    new file for class path

---
+++ nat/java_util_VMTimeZone.h
@@ -0,0 +1,21 @@
+/* This file is machine generated, don't edit it !*/
+
+#ifndef _JAVA_UTIL_VMTIMEZONE_H
+#define _JAVA_UTIL_VMTIMEZONE_H
+
+/* Structure information for class: java/util/VMTimeZone */
+
+typedef struct java_util_VMTimeZone {
+   java_objectheader header;
+} java_util_VMTimeZone;
+
+
+/*
+ * Class:     java/util/VMTimeZone
+ * Method:    getSystemTimeZoneId
+ * Signature: ()Ljava/lang/String;
+ */
+JNIEXPORT struct java_lang_String* JNICALL Java_java_util_VMTimeZone_getSystemTimeZoneId(JNIEnv *env, jclass clazz);
+
+#endif
+
