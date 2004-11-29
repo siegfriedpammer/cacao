2c30361f23a94c63fb13563f8c0db056229965ff
Christian Thalinger <twisti@complang.tuwien.ac.at>
2004-11-29 16:10:33 +0000

    New source tree.Constructor.c Field.c FileChannelImpl.c GtkComponentPeer.c GtkFileDialogPeer.c GtkScrollPanePeer.c JOWENNTest1.c Makefile.am Method.c Proxy.c Runtime.c Thread.c VMClass.c VMClassLoader.c VMObject.c VMObjectStreamClass.c VMSecurityManager.c VMString.c VMSystem.c VMThrowable.c classcontextnativeTest.c classcontextnativeTest.h java_lang_ClassLoader.h java_lang_Cloneable.h java_lang_Object.h java_lang_String.h java_lang_Thread.h java_lang_ThreadGroup.h java_lang_Throwable.h java_lang_VMObject.h java_lang_VMThread.h

--- nat/GtkFileDialogPeer.c
+++
@@ -1,8 +0,0 @@
-/*
- * Class:     gnu/java/awt/peer/gtk/GtkFileDialogPeer
- * Method:    setFile
- * Signature: (Ljava/lang/String;)V
- */
-JNIEXPORT void JNICALL Java_gnu_java_awt_peer_gtk_GtkFileDialogPeer_setFile (JNIEnv *env ,  struct 
-gnu_java_awt_peer_gtk_GtkFileDialogPeer* this , struct java_lang_String* par1) {
-}
