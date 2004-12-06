24e32685c3a04b4a59f4829612732eb99cf293f4
Christian Thalinger <twisti@complang.tuwien.ac.at>
2004-12-06 12:30:35 +0000

    New header.

---
+++ src/vm/jit/stacktrace.h
@@ -0,0 +1,59 @@
+/* vm/jit/stacktrace.h - header file for stacktrace generation
+
+   Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003
+   R. Grafl, A. Krall, C. Kruegel, C. Oates, R. Obermaisser,
+   M. Probst, S. Ring, E. Steiner, C. Thalinger, D. Thuernbeck,
+   P. Tomsich, J. Wenninger
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+
+   Contact: cacao@complang.tuwien.ac.at
+
+   Authors: Christian Thalinger
+
+   Changes:
+
+   $Id: stacktrace.h 1693 2004-12-06 12:30:35Z twisti $
+
+*/
+
+
+#ifndef _STACKTRACE_H
+#define _STACKTRACE_H
+
+/* function prototypes ********************************************************/
+
+void cacao_stacktrace_NormalTrace(void **target);
+java_objectarray *cacao_createClassContextArray();
+java_objectheader *cacao_currentClassLoader();
+
+#endif /* _STACKTRACE_H */
+
+
+/*
+ * These are local overrides for various environment variables in Emacs.
+ * Please do not remove this and leave it at the end of the file, where
+ * Emacs will automagically detect them.
+ * ---------------------------------------------------------------------
+ * Local variables:
+ * mode: c
+ * indent-tabs-mode: t
+ * c-basic-offset: 4
+ * tab-width: 4
+ * End:
+ */
