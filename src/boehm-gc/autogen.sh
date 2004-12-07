0422bb4f8c7cfd5af861c2d6b49fdcc948752267
Joseph Wenninger <joseph@complang.tuwien.ac.at>
2004-12-07 07:55:07 +0000

    autogen.sh without typo

--- src/boehm-gc/autogen.sh
+++ src/boehm-gc/autogen.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 libtoolize --automake
-if test `uname` == 'FreeBSD'; then
-	echo "FreeBSD (only tested with 5.3-RELEASE though"
+if test `uname` = 'FreeBSD'; then
+	echo "FreeBSD -- only tested with 5.3-RELEASE though"
 	aclocal  -I . -I /usr/local/share/aclocal -I /usr/local/share/aclocal19
 else
 	aclocal
