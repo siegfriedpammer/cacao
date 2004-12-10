fbd9688edf4048adaecde27528451319cdc96ae2
Christian Thalinger <twisti@complang.tuwien.ac.at>
2004-12-10 13:10:29 +0000

    More generic version, more tests.

--- tests/native/testarguments.c
+++ tests/native/testarguments.c
@@ -1,8 +1,71 @@
-#include "jni.h"
+#include "native/jni.h"
 #include "testarguments.h"
 
-JNIEXPORT void JNICALL Java_testarguments_nsub(JNIEnv *env, jclass clazz)
+
+JNIEXPORT void JNICALL Java_testarguments_nisub(JNIEnv *env, jclass clazz, int a, int b, int c, int d, int e, int f, int g, int h, int i, int j)
 {
-  (*env)->GetVersion(env);
+    jmethodID mid;
+
+    printf("native: int: %d %d %d %d %d %d %d %d %d %d\n", a, b, c, d, e, f, g, h, i, j);
+
+    mid = (*env)->GetStaticMethodID(env, clazz, "jisub", "(IIIIIIIIII)V");
+
+    if (mid == 0) {
+        printf("native: couldn't find jisub\n");
+        return;
+    }
+
+/*      (*env)->CallVoidMethod(env, clazz, mid, a, b, c, d, e, f, g, h, i, j); */
+    (*env)->CallStaticVoidMethod(env, clazz, mid, a, b, c, d, e, f, g, h, i, j);
 }
 
+
+JNIEXPORT void JNICALL Java_testarguments_nlsub(JNIEnv *env, jclass clazz, long a, long b, long c, long d, long e, long f, long g, long h, long i, long j)
+{
+    jmethodID mid;
+
+    printf("native: long: %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld\n", a, b, c, d, e, f, g, h, i, j);
+
+    mid = (*env)->GetStaticMethodID(env, clazz, "jlsub", "(JJJJJJJJJJ)V");
+
+    if (mid == 0) {
+        printf("native: couldn't find jlsub\n");
+        return;
+    }
+
+    (*env)->CallStaticVoidMethod(env, clazz, mid, a, b, c, d, e, f, g, h, i, j);
+}
+
+
+JNIEXPORT void JNICALL Java_testarguments_nfsub(JNIEnv *env, jclass clazz, float a, float b, float c, float d, float e, float f, float g, float h, float i, float j)
+{
+    jmethodID mid;
+
+    printf("native: float: %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f\n", a, b, c, d, e, f, g, h, i, j);
+
+    mid = (*env)->GetStaticMethodID(env, clazz, "jfsub", "(FFFFFFFFFF)V");
+
+    if (mid == 0) {
+        printf("native: couldn't find jfsub\n");
+        return;
+    }
+
+    (*env)->CallStaticVoidMethod(env, clazz, mid, a, b, c, d, e, f, g, h, i, j);
+}
+
+
+JNIEXPORT void JNICALL Java_testarguments_ndsub(JNIEnv *env, jclass clazz, double a, double b, double c, double d, double e, double f, double g, double h, double i, double j)
+{
+    jmethodID mid;
+
+    printf("native: double: %.16g %.16g %.16g %.16g %.16g %.16g %.16g %.16g %.16g %.16g\n", a, b, c, d, e, f, g, h, i, j);
+
+    mid = (*env)->GetStaticMethodID(env, clazz, "jdsub", "(DDDDDDDDDD)V");
+
+    if (mid == 0) {
+        printf("native: couldn't find jfsub\n");
+        return;
+    }
+
+    (*env)->CallStaticVoidMethod(env, clazz, mid, a, b, c, d, e, f, g, h, i, j);
+}
--- tests/native/testarguments.java
+++ tests/native/testarguments.java
@@ -1,15 +1,73 @@
 import java.util.*;
 
 public class testarguments {
-    public static native void nsub();
+    static Random r;
+
+    public static native void nisub(int a, int b, int c, int d, int e,
+                                    int f, int g, int h, int i, int j);
+    public static native void nlsub(long a, long b, long c, long d, long e,
+                                    long f, long g, long h, long i, long j);
+    public static native void nfsub(float a, float b, float c, float d, float e,
+                                    float f, float g, float h, float i, float j);
+    public static native void ndsub(double a, double b, double c, double d, double e,
+                                    double f, double g, double h, double i, double j);
 
     public static void main(String[] argv) {
-        Random r = new Random();
+        r = new Random(0);
 
         System.loadLibrary("testarguments");
-        nsub();
+
+//          nisub(i(), i(), i(), i(), i(), i(), i(), i(), i(), i());
+//          nlsub(l(), l(), l(), l(), l(), l(), l(), l(), l(), l());
+        nfsub(f(), f(), f(), f(), f(), f(), f(), f(), f(), f());
+//          ndsub(d(), d(), d(), d(), d(), d(), d(), d(), d(), d());
+
+//          try {
+//              ndsub(d(), d(), d(), d(), d(), d(), d(), d(), d(), d());
+//          } catch (LinkageError e) {
+//              System.out.println("catched");
+//          }
     }
 
-    public void jsub(int a0, float fa0) {
+    public static void jisub(int a, int b, int c, int d, int e,
+                             int f, int g, int h, int i, int j) {
+        p("java  : int:");
+        p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
+        pln();
     }
+
+    public static void jlsub(long a, long b, long c, long d, long e,
+                             long f, long g, long h, long i, long j) {
+        p("java  : long:");
+        p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
+        pln();
+    }
+
+    public static void jfsub(float a, float b, float c, float d, float e,
+                             float f, float g, float h, float i, float j) {
+        p("java  : float:");
+        p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
+        pln();
+    }
+
+    public static void jdsub(double a, double b, double c, double d, double e,
+                             double f, double g, double h, double i, double j) {
+        p("java  : double:");
+        p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
+        pln();
+    }
+
+    static int i() { return r.nextInt(); }
+    static long l() { return r.nextLong(); }
+    static float f() { return r.nextFloat(); }
+    static double d() { return r.nextDouble(); }
+
+    static void p(String s) { System.out.print(s); }
+
+    static void p(int i) { System.out.print(" " + i); }
+    static void p(long l) { System.out.print(" " + l); }
+    static void p(float f) { System.out.print(" " + f); }
+    static void p(double d) { System.out.print(" " + d); }
+
+    static void pln() { System.out.println(); }
 }
