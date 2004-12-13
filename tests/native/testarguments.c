f62bfb9c8e3d52b33dbfde6418e682a11c89b974
Christian Thalinger <twisti@complang.tuwien.ac.at>
2004-12-13 10:09:35 +0000

    - more checks - gnu header

--- tests/native/testarguments.c
+++ tests/native/testarguments.c
@@ -1,12 +1,48 @@
+/* tests/native/testarguments.c - tests argument passing
+
+   Copyright (C) 1996-2005 R. Grafl, A. Krall, C. Kruegel, C. Oates,
+   R. Obermaisser, M. Platter, M. Probst, S. Ring, E. Steiner,
+   C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich, J. Wenninger,
+   TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+
+   Contact: cacao@complang.tuwien.ac.at
+
+   Authors: Christian Thalinger
+
+   $Id: testarguments.c 1756 2004-12-13 10:09:35Z twisti $
+
+*/
+
+
+#include <stdio.h>
+
+#include "config.h"
 #include "native/jni.h"
-#include "testarguments.h"
 
 
-JNIEXPORT void JNICALL Java_testarguments_nisub(JNIEnv *env, jclass clazz, int a, int b, int c, int d, int e, int f, int g, int h, int i, int j)
+JNIEXPORT void JNICALL Java_testarguments_nisub(JNIEnv *env, jclass clazz, jint a, jint b, jint c, jint d, jint e, jint f, jint g, jint h, jint i, jint j)
 {
     jmethodID mid;
 
-    printf("native: int: %d %d %d %d %d %d %d %d %d %d\n", a, b, c, d, e, f, g, h, i, j);
+    printf("java-native: %d %d %d %d %d %d %d %d %d %d\n", a, b, c, d, e, f, g, h, i, j);
+    fflush(stdout);
 
     mid = (*env)->GetStaticMethodID(env, clazz, "jisub", "(IIIIIIIIII)V");
 
@@ -20,11 +56,16 @@ JNIEXPORT void JNICALL Java_testarguments_nisub(JNIEnv *env, jclass clazz, int a
 }
 
 
-JNIEXPORT void JNICALL Java_testarguments_nlsub(JNIEnv *env, jclass clazz, long a, long b, long c, long d, long e, long f, long g, long h, long i, long j)
+JNIEXPORT void JNICALL Java_testarguments_nlsub(JNIEnv *env, jclass clazz, jlong a, jlong b, jlong c, jlong d, jlong e, jlong f, jlong g, jlong h, jlong i, jlong j)
 {
     jmethodID mid;
 
-    printf("native: long: %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld\n", a, b, c, d, e, f, g, h, i, j);
+#if defined(__I386__) || defined(__POWERPC__)
+    printf("java-native: %lld %lld %lld %lld %lld %lld %lld %lld %lld %lld\n", a, b, c, d, e, f, g, h, i, j);
+#else
+    printf("java-native: %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld\n", a, b, c, d, e, f, g, h, i, j);
+#endif
+    fflush(stdout);
 
     mid = (*env)->GetStaticMethodID(env, clazz, "jlsub", "(JJJJJJJJJJ)V");
 
@@ -37,11 +78,12 @@ JNIEXPORT void JNICALL Java_testarguments_nlsub(JNIEnv *env, jclass clazz, long 
 }
 
 
-JNIEXPORT void JNICALL Java_testarguments_nfsub(JNIEnv *env, jclass clazz, float a, float b, float c, float d, float e, float f, float g, float h, float i, float j)
+JNIEXPORT void JNICALL Java_testarguments_nfsub(JNIEnv *env, jclass clazz, jfloat a, jfloat b, jfloat c, jfloat d, jfloat e, jfloat f, jfloat g, jfloat h, jfloat i, jfloat j)
 {
     jmethodID mid;
 
-    printf("native: float: %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f\n", a, b, c, d, e, f, g, h, i, j);
+    printf("java-native: %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f %.8f\n", a, b, c, d, e, f, g, h, i, j);
+    fflush(stdout);
 
     mid = (*env)->GetStaticMethodID(env, clazz, "jfsub", "(FFFFFFFFFF)V");
 
@@ -54,11 +96,12 @@ JNIEXPORT void JNICALL Java_testarguments_nfsub(JNIEnv *env, jclass clazz, float
 }
 
 
-JNIEXPORT void JNICALL Java_testarguments_ndsub(JNIEnv *env, jclass clazz, double a, double b, double c, double d, double e, double f, double g, double h, double i, double j)
+JNIEXPORT void JNICALL Java_testarguments_ndsub(JNIEnv *env, jclass clazz, jdouble a, jdouble b, jdouble c, jdouble d, jdouble e, jdouble f, jdouble g, jdouble h, jdouble i, jdouble j)
 {
     jmethodID mid;
 
-    printf("native: double: %.16g %.16g %.16g %.16g %.16g %.16g %.16g %.16g %.16g %.16g\n", a, b, c, d, e, f, g, h, i, j);
+    printf("java-native: %.16g %.16g %.16g %.16g %.16g %.16g %.16g %.16g %.16g %.16g\n", a, b, c, d, e, f, g, h, i, j);
+    fflush(stdout);
 
     mid = (*env)->GetStaticMethodID(env, clazz, "jdsub", "(DDDDDDDDDD)V");
 
--- tests/native/testarguments.java
+++ tests/native/testarguments.java
@@ -1,3 +1,36 @@
+/* tests/native/testarguments.java - tests argument passing
+
+   Copyright (C) 1996-2005 R. Grafl, A. Krall, C. Kruegel, C. Oates,
+   R. Obermaisser, M. Platter, M. Probst, S. Ring, E. Steiner,
+   C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich, J. Wenninger,
+   TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+
+   Contact: cacao@complang.tuwien.ac.at
+
+   Authors: Christian Thalinger
+
+   $Id: testarguments.java 1756 2004-12-13 10:09:35Z twisti $
+
+*/
+
+
 import java.util.*;
 
 public class testarguments {
@@ -17,10 +50,20 @@ public class testarguments {
 
         System.loadLibrary("testarguments");
 
-//          nisub(i(), i(), i(), i(), i(), i(), i(), i(), i(), i());
-//          nlsub(l(), l(), l(), l(), l(), l(), l(), l(), l(), l());
-        nfsub(f(), f(), f(), f(), f(), f(), f(), f(), f(), f());
-//          ndsub(d(), d(), d(), d(), d(), d(), d(), d(), d(), d());
+        pln("testing int --------------------------------------------------");
+        isub(i(), i(), i(), i(), i(), i(), i(), i(), i(), i());
+        pln();
+
+        pln("testing long -------------------------------------------------");
+        lsub(l(), l(), l(), l(), l(), l(), l(), l(), l(), l());
+        pln();
+
+        pln("testing float ------------------------------------------------");
+        fsub(f(), f(), f(), f(), f(), f(), f(), f(), f(), f());
+        pln();
+
+        pln("testing double -----------------------------------------------");
+        dsub(d(), d(), d(), d(), d(), d(), d(), d(), d(), d());
 
 //          try {
 //              ndsub(d(), d(), d(), d(), d(), d(), d(), d(), d(), d());
@@ -29,33 +72,76 @@ public class testarguments {
 //          }
     }
 
+
+    // test java-java argument passing
+
+    public static void isub(int a, int b, int c, int d, int e,
+                            int f, int g, int h, int i, int j) {
+        p("java-java  :");
+        p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
+        pln();
+
+        nisub(a, b, c, d, e, f, g, h, i, j);
+    }
+
+    public static void lsub(long a, long b, long c, long d, long e,
+                            long f, long g, long h, long i, long j) {
+        p("java-java  :");
+        p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
+        pln();
+
+        nlsub(a, b, c, d, e, f, g, h, i, j);
+    }
+
+    public static void fsub(float a, float b, float c, float d, float e,
+                            float f, float g, float h, float i, float j) {
+        p("java-java  :");
+        p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
+        pln();
+
+        nfsub(a, b, c, d, e, f, g, h, i, j);
+    }
+
+    public static void dsub(double a, double b, double c, double d, double e,
+                            double f, double g, double h, double i, double j) {
+        p("java-java  :");
+        p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
+        pln();
+
+        ndsub(a, b, c, d, e, f, g, h, i, j);
+    }
+
+
+    // test native-java argument passing
+
     public static void jisub(int a, int b, int c, int d, int e,
                              int f, int g, int h, int i, int j) {
-        p("java  : int:");
+        p("native-java:");
         p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
         pln();
     }
 
     public static void jlsub(long a, long b, long c, long d, long e,
                              long f, long g, long h, long i, long j) {
-        p("java  : long:");
+        p("native-java:");
         p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
         pln();
     }
 
     public static void jfsub(float a, float b, float c, float d, float e,
                              float f, float g, float h, float i, float j) {
-        p("java  : float:");
+        p("native-java:");
         p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
         pln();
     }
 
     public static void jdsub(double a, double b, double c, double d, double e,
                              double f, double g, double h, double i, double j) {
-        p("java  : double:");
+        p("native-java:");
         p(a); p(b); p(c); p(d); p(e); p(f); p(g); p(h); p(i); p(j);
         pln();
     }
+
 
     static int i() { return r.nextInt(); }
     static long l() { return r.nextLong(); }
@@ -70,4 +156,5 @@ public class testarguments {
     static void p(double d) { System.out.print(" " + d); }
 
     static void pln() { System.out.println(); }
+    static void pln(String s) { System.out.println(s); }
 }
