0d9f54047c2c9f579905a680a3cf0b24110ed51e
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-02-08 13:43:33 +0000

    Added test files for testing native stuff more easily.

---
+++ tests/native/test.c
@@ -0,0 +1,44 @@
+/* tests/native/test.c - for testing native stuff
+
+   Copyright (C) 1996-2005 R. Grafl, A. Krall, C. Kruegel, C. Oates,
+   R. Obermaisser, M. Platter, M. Probst, S. Ring, E. Steiner,
+   C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich, J. Wenninger,
+   TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+
+   Contact: cacao@complang.tuwien.ac.at
+
+   Authors: Christian Thalinger
+
+   $Id: test.c 1899 2005-02-08 13:43:33Z twisti $
+
+*/
+
+
+#include <stdio.h>
+
+#include "config.h"
+#include "native/jni.h"
+
+
+JNIEXPORT void JNICALL Java_test_nsub(JNIEnv *env, jclass clazz)
+{
+    jint version = (*env)->GetVersion(env);
+    printf("JNI Version: %d\n", version);
+}
---
+++ tests/native/test.java
@@ -0,0 +1,41 @@
+/* tests/native/test.java - file for testing native related stuff
+
+   Copyright (C) 1996-2005 R. Grafl, A. Krall, C. Kruegel, C. Oates,
+   R. Obermaisser, M. Platter, M. Probst, S. Ring, E. Steiner,
+   C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich, J. Wenninger,
+   TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+
+   Contact: cacao@complang.tuwien.ac.at
+
+   Authors: Christian Thalinger
+
+   $Id: test.java 1899 2005-02-08 13:43:33Z twisti $
+
+*/
+
+
+public class test {
+    public static native void nsub();
+
+    public static void main(String[] argv) {
+        System.loadLibrary("test");
+        nsub();
+    }
+}
