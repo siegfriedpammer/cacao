43f2b424ddc057dd08564bdf1342c6cc256e07c4
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-03-28 21:56:52 +0000

    Field related stuff (mostly from loader.c/global.h).

---
+++ src/vm/field.c
@@ -0,0 +1,90 @@
+/* src/vm/field.c - field functions
+
+   Copyright (C) 1996-2005 R. Grafl, A. Krall, C. Kruegel, C. Oates,
+   R. Obermaisser, M. Platter, M. Probst, S. Ring, E. Steiner,
+   C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich, J. Wenninger,
+   Institut f. Computersprachen - TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+
+   Contact: cacao@complang.tuwien.ac.at
+
+   Authors: Reinhard Grafl
+
+   Changes: Andreas Krall
+            Roman Obermaiser
+            Mark Probst
+            Edwin Steiner
+            Christian Thalinger
+
+   $Id: field.c 2099 2005-03-28 21:56:52Z twisti $
+
+*/
+
+
+#include "arch.h"
+#include "types.h"
+#include "vm/exceptions.h"
+#include "vm/global.h"
+#include "vm/loader.h"
+#include "vm/options.h"
+#include "vm/stringlocal.h"
+
+
+/* field_free ******************************************************************
+
+   Frees a fields' resources.
+
+*******************************************************************************/
+
+void field_free(fieldinfo *f)
+{
+	/* empty */
+}
+
+
+/* field_display ***************************************************************
+
+   (debugging only)
+
+*******************************************************************************/
+
+void field_display(fieldinfo *f)
+{
+	printf("   ");
+	printflags(f->flags);
+	printf(" ");
+	utf_display(f->name);
+	printf(" ");
+	utf_display(f->descriptor);	
+	printf(" offset: %ld\n", (long int) (f->offset));
+}
+
+
+/*
+ * These are local overrides for various environment variables in Emacs.
+ * Please do not remove this and leave it at the end of the file, where
+ * Emacs will automagically detect them.
+ * ---------------------------------------------------------------------
+ * Local variables:
+ * mode: c
+ * indent-tabs-mode: t
+ * c-basic-offset: 4
+ * tab-width: 4
+ * End:
+ */
---
+++ src/vm/field.h
@@ -0,0 +1,90 @@
+/* src/vm/field.h - field functions header
+
+   Copyright (C) 1996-2005 R. Grafl, A. Krall, C. Kruegel, C. Oates,
+   R. Obermaisser, M. Platter, M. Probst, S. Ring, E. Steiner,
+   C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich, J. Wenninger,
+   Institut f. Computersprachen - TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+
+   Contact: cacao@complang.tuwien.ac.at
+
+   Authors: Reinhard Grafl
+
+   Changes: Christian Thalinger
+
+   $Id: field.h 2099 2005-03-28 21:56:52Z twisti $
+*/
+
+
+#ifndef _FIELD_H
+#define _FIELD_H
+
+/* forward typedefs ***********************************************************/
+
+typedef struct fieldinfo fieldinfo; 
+
+
+#include "types.h"
+#include "vm/global.h"
+#include "vm/loader.h"
+#include "vm/utf8.h"
+#include "vm/jit/inline/parseXTA.h"
+
+
+/* fieldinfo ******************************************************************/
+
+struct fieldinfo {	      /* field of a class                                 */
+	s4         flags;     /* ACC flags                                        */
+	s4         type;      /* basic data type                                  */
+	utf       *name;      /* name of field                                    */
+	utf       *descriptor;/* JavaVM descriptor string of field                */
+	typedesc  *parseddesc;/* parsed descriptor                                */
+
+	s4         offset;    /* offset from start of object (instance variables) */
+
+	imm_union  value;     /* storage for static values (class variables)      */
+
+	classinfo *class;     /* needed by typechecker. Could be optimized        */
+	                      /* away by using constant_FMIref instead of         */
+	                      /* fieldinfo throughout the compiler.               */
+
+	xtafldinfo *xta;
+};
+
+
+/* function prototypes ********************************************************/
+
+void field_free(fieldinfo *f);
+void field_display(fieldinfo *f);
+
+#endif /* _LOADER_H */
+
+
+/*
+ * These are local overrides for various environment variables in Emacs.
+ * Please do not remove this and leave it at the end of the file, where
+ * Emacs will automagically detect them.
+ * ---------------------------------------------------------------------
+ * Local variables:
+ * mode: c
+ * indent-tabs-mode: t
+ * c-basic-offset: 4
+ * tab-width: 4
+ * End:
+ */
