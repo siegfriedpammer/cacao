7bae96c0a6f6ac772d80e8d2e6d9f14065f8b21c
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-06-08 19:30:09 +0000

    Moved ABI stuff into md-abi.h

--- src/vm/jit/alpha/arch.h
+++ src/vm/jit/alpha/arch.h
@@ -28,7 +28,7 @@
 
    Changes:
 
-   $Id: arch.h 2357 2005-04-22 17:37:57Z christian $
+   $Id: arch.h 2609 2005-06-08 19:30:09Z twisti $
 
 */
 
@@ -36,51 +36,8 @@
 #ifndef _ARCH_H
 #define _ARCH_H
 
-
-/* preallocated registers *****************************************************/
-
-/* integer registers */
-  
-#define REG_RESULT      0    /* to deliver method results                     */
-
-#define REG_RA          26   /* return address                                */
-#define REG_PV          27   /* procedure vector, must be provided by caller  */
-#define REG_METHODPTR   28   /* pointer to the place from where the procedure */
-                             /* vector has been fetched                       */
-#define REG_ITMP1       25   /* temporary register                            */
-#define REG_ITMP2       28   /* temporary register and method pointer         */
-#define REG_ITMP3       29   /* temporary register                            */
-
-#define REG_ITMP1_XPTR  25   /* exception pointer = temporary register 1      */
-#define REG_ITMP2_XPC   28   /* exception pc = temporary register 2           */
-
-#define REG_SP          30   /* stack pointer                                 */
-#define REG_ZERO        31   /* always zero                                   */
-
-/* floating point registers */
-
-#define REG_FRESULT     0    /* to deliver floating point method results      */
-
-#define REG_FTMP1       28   /* temporary floating point register             */
-#define REG_FTMP2       29   /* temporary floating point register             */
-#define REG_FTMP3       30   /* temporary floating point register             */
-
-#define REG_IFTMP       28   /* temporary integer and floating point register */
-
-
-#define INT_SAV_CNT      7   /* number of int callee saved registers          */
-#define INT_ARG_CNT      6   /* number of int argument registers              */
-
-#define FLT_SAV_CNT      8   /* number of flt callee saved registers          */
-#define FLT_ARG_CNT      6   /* number of flt argument registers              */
-
-
-#define TRACE_ARGS_NUM  6
-
-
 /* define architecture features ***********************************************/
 
-#define POINTERSIZE                      8
 #define WORDS_BIGENDIAN                  0
 
 #define U8_AVAILABLE                     1
@@ -114,9 +71,6 @@
                                             /* only store REG_ZERO            */
 
 #define CONDITIONAL_LOADCONST            1
-
-/*  #define CONSECUTIVE_INTEGER_ARGS */
-/*  #define CONSECUTIVE_FLOAT_ARGS */
 
 #endif /* _ARCH_H */
 
