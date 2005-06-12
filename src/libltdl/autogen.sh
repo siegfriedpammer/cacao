880860c9dcbbfc696d01dbfb8fea55af7a7607de
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-06-12 23:21:27 +0000

    libltdl from GNU Libtool 1.5.18

---
+++ src/libltdl/autogen.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+libtoolize --automake
+if test `uname` = 'FreeBSD'; then
+	echo "FreeBSD -- only tested with 5.3-RELEASE though"
+        aclocal -I . -I /usr/local/share/aclocal -I /usr/local/share/aclocal19
+else
+        aclocal -I .
+fi
+autoheader
+automake --add-missing
+autoconf
