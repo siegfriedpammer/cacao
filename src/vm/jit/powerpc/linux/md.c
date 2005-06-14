b22721ce0d90862a9fc185eb8eaced3763ddac5f
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-06-14 17:18:38 +0000

    * Moved signal stuff

--- src/vm/jit/powerpc/linux/md.c
+++ src/vm/jit/powerpc/linux/md.c
@@ -28,11 +28,87 @@
 
    Changes:
 
-   $Id: md.c 2554 2005-06-06 14:48:21Z twisti $
+   $Id: md.c 2682 2005-06-14 17:18:38Z twisti $
 
 */
 
 
+#include <stdlib.h>
+#include <signal.h>
+#include <ucontext.h>
+
+#include "vm/jit/powerpc/types.h"
+#include "vm/jit/powerpc/linux/md-abi.h"
+
+#include "vm/exceptions.h"
+#include "vm/global.h"
+#include "vm/options.h"
+#include "vm/jit/asmpart.h"
+
+
+/* catch_NullPointerException **************************************************
+
+   NullPointerException signal handler for hardware null pointer check.
+
+*******************************************************************************/
+
+void catch_NullPointerException(int sig, siginfo_t *siginfo, void *_p)
+{
+	ucontext_t       *uc;
+	mcontext_t       *mc;
+	struct sigaction  act;
+	sigset_t          nsig;
+
+	java_objectheader *xptr;
+
+ 	uc = (ucontext_t *) _p;
+ 	mc = _uc->uc_mcontext.uc_regs;
+
+	act.sa_sigaction = catch_NullPointerException;
+	act.sa_flags = SA_SIGINFO;
+	sigaction(sig, &act, NULL);
+
+	xptr = new_nullpointerexception();
+
+	sigemptyset(&nsig);
+	sigaddset(&nsig, sig);
+	sigprocmask(SIG_UNBLOCK, &nsig, NULL);               /* unblock signal    */
+
+	mc->gregs[REG_ITMP1_XPTR] = (ptrint) xptr;
+	mc->gregs[REG_ITMP2_XPC] = mc->gregs[PT_NIP];
+	mc->gregs[PT_NIP] = (ptrint) asm_handle_exception;
+}
+
+
+/* init_exceptions *************************************************************
+
+   Installs the OS dependent signal handlers.
+
+*******************************************************************************/
+
+void init_exceptions(void)
+{
+	struct sigaction act;
+
+	GC_dirty_init(1);
+
+	/* install signal handlers we need to convert to exceptions */
+
+	sigemptyset(&act.sa_mask);
+
+	if (!checknull) {
+		act.sa_sigaction = catch_NullPointerException;
+		act.sa_flags = SA_SIGINFO;
+
+#if defined(SIGSEGV)
+		sigaction(SIGSEGV, &act, NULL);
+#endif
+
+#if defined(SIGBUS)
+		sigaction(SIGBUS, &act, NULL);
+#endif
+	}
+}
 
 
 /*
