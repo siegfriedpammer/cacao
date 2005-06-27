b2807e43a41466b830f8211fb95069ceeb9c51b1
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-06-27 19:58:48 +0000

    * Ooops, a darwin accident

--- src/boehm-gc/autogen.sh
+++ src/boehm-gc/autogen.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-glibtoolize --automake
+libtoolize --automake
 if test `uname` = 'FreeBSD'; then
-	echo "FreeBSD -- only tested with 5.3-RELEASE though"
-	aclocal  -I . -I /usr/local/share/aclocal -I /usr/local/share/aclocal19
+    aclocal  -I . -I /usr/local/share/aclocal -I /usr/local/share/aclocal19
 else
-	aclocal
+    aclocal
 fi
 autoheader
 automake --add-missing
