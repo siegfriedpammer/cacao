0767987d004e0e621cb5b38522f8af278529d3f5
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-07-19 08:51:41 +0000

    * Ok, now the test is correct

--- tests/regression/clinitexception.java
+++ tests/regression/clinitexception.java
@@ -1,17 +1,9 @@
 public class clinitexception {
-    static class ThrowRuntimeException {
-        static {
-            if (true)
-                throw new RuntimeException();
-        }
+    static {
+        if (true)
+            throw new RuntimeException();
     }
 
     public static void main(String[] argv) {
-        try {
-            new ThrowRuntimeException();
-            System.out.println("FAILED");
-        } catch (ExceptionInInitializerError e) {
-            System.out.println("OK");
-        }
     }
 }
