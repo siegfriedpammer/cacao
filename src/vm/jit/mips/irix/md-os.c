deaa06e27dfb186c12fa56f01bc68d46e13695f2
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-07-27 12:14:03 +0000

    * Added irix subdirectory.

---
+++ src/vm/jit/mips/irix/md-os.c
@@ -0,0 +1,149 @@
+/* src/vm/jit/mips/irix/md-os.c - machine dependent MIPS IRIX functions
+
+   Copyright (C) 1996-2005 R. Grafl, A. Krall, C. Kruegel, C. Oates,
+   R. Obermaisser, M. Platter, M. Probst, S. Ring, E. Steiner,
+   C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich, J. Wenninger,
+   Institut f. Computersprachen - TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+
+   Contact: cacao@complang.tuwien.ac.at
+
+   Authors: Andreas Krall
+            Reinhard Grafl
+
+   Changes: Christian Thalinger
+
+   $Id: md-os.c 3116 2005-07-27 12:14:03Z twisti $
+
+*/
+
+
+#include <assert.h>
+#include <signal.h>
+#include <sys/fpu.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#include "config.h"
+
+#include "vm/jit/mips/md-abi.h"
+#include "vm/jit/mips/types.h"
+
+#include "mm/boehm.h"
+#include "vm/exceptions.h"
+#include "vm/stringlocal.h"
+#include "vm/jit/asmpart.h"
+#include "vm/jit/stacktrace.h"
+
+
+/* md_init *********************************************************************
+
+   Do some machine dependent initialization.
+
+*******************************************************************************/
+
+void md_init(void)
+{
+	/* The Boehm GC initialization blocks the SIGSEGV signal. So we do a      */
+	/* dummy allocation here to ensure that the GC is initialized.            */
+
+	heap_allocate(1, 0, NULL);
+
+
+	/* Turn off flush-to-zero */
+
+	{
+		union fpc_csr n;
+		n.fc_word = get_fpc_csr();
+		n.fc_struct.flush = 0;
+		set_fpc_csr(n.fc_word);
+	}
+}
+
+
+/* signal_handler_sigsegv ******************************************************
+
+   NullPointerException signal handler for hardware null pointer check.
+
+*******************************************************************************/
+
+void signal_handler_sigsegv(int sig, siginfo_t *siginfo, void *_p)
+{
+	ucontext_t  *_uc;
+	mcontext_t  *_mc;
+	u4           instr;
+	ptrint       addr;
+	u1          *pv;
+	u1          *sp;
+	functionptr  ra;
+	functionptr  xpc;
+
+	_uc = (struct ucontext *) _p;
+	_mc = &_uc->uc_mcontext;
+
+	instr = *((u4 *) (_mc->gregs[CTX_EPC]));
+	addr = _mc->gregs[(instr >> 21) & 0x1f];
+
+	if (addr == 0) {
+		pv  = (u1 *) _mc->gregs[REG_PV];
+		sp  = (u1 *) _mc->gregs[REG_SP];
+		ra  = (functionptr) _mc->gregs[REG_RA]; /* this is correct for leafs*/
+		xpc = (functionptr) _mc->gregs[CTX_EPC];
+
+		_mc->gregs[REG_ITMP1_XPTR] =
+			(ptrint) stacktrace_hardware_nullpointerexception(pv, sp, ra, xpc);
+
+		_mc->gregs[REG_ITMP2_XPC] = (ptrint) xpc;
+		_mc->gregs[CTX_EPC] = (ptrint) asm_handle_exception;
+
+	} else {
+        addr += (long) ((instr << 16) >> 16);
+
+		throw_cacao_exception_exit(string_java_lang_InternalError,
+								   "faulting address: 0x%lx at 0x%lx\n",
+								   addr, _mc->gregs[CTX_EPC]);
+	}
+}
+
+
+#if defined(USE_THREADS) && defined(NATIVE_THREADS)
+void thread_restartcriticalsection(ucontext_t *uc)
+{
+	void *critical;
+
+	critical = thread_checkcritical((void*) uc->uc_mcontext.gregs[CTX_EPC]);
+
+	if (critical)
+		uc->uc_mcontext.gregs[CTX_EPC] = (ptrint) critical;
+}
+#endif
+
+
+/*
+ * These are local overrides for various environment variables in Emacs.
+ * Please do not remove this and leave it at the end of the file, where
+ * Emacs will automagically detect them.
+ * ---------------------------------------------------------------------
+ * Local variables:
+ * mode: c
+ * indent-tabs-mode: t
+ * c-basic-offset: 4
+ * tab-width: 4
+ * End:
+ */
--- src/vm/jit/mips/md.c
+++ src/vm/jit/mips/md.c
@@ -24,114 +24,24 @@
 
    Contact: cacao@complang.tuwien.ac.at
 
-   Authors: Andreas Krall
-            Reinhard Grafl
+   Authors: Christian Thalinger
 
-   Changes: Christian Thalinger
+   Changes: 
 
-   $Id: md.c 3112 2005-07-27 10:35:06Z twisti $
+   $Id: md.c 3116 2005-07-27 12:14:03Z twisti $
 
 */
 
 
 #include <assert.h>
-#include <signal.h>
-#include <sys/fpu.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
 #include "config.h"
 
-#include "vm/jit/mips/md-abi.h"
 #include "vm/jit/mips/types.h"
 
-#include "vm/exceptions.h"
-#include "vm/stringlocal.h"
-#include "vm/jit/asmpart.h"
-#include "vm/jit/stacktrace.h"
-
-
-/* md_init *********************************************************************
-
-   Do some machine dependent initialization.
-
-*******************************************************************************/
-
-void md_init(void)
-{
-	/* The Boehm GC initialization blocks the SIGSEGV signal. So we do a      */
-	/* dummy allocation here to ensure that the GC is initialized.            */
-
-	heap_allocate(1, 0, NULL);
-
-
-	/* Turn off flush-to-zero */
-
-	{
-		union fpc_csr n;
-		n.fc_word = get_fpc_csr();
-		n.fc_struct.flush = 0;
-		set_fpc_csr(n.fc_word);
-	}
-}
-
-
-/* signal_handler_sigsegv ******************************************************
-
-   NullPointerException signal handler for hardware null pointer check.
-
-*******************************************************************************/
-
-void signal_handler_sigsegv(int sig, siginfo_t *siginfo, void *_p)
-{
-	ucontext_t  *_uc;
-	mcontext_t  *_mc;
-	u4           instr;
-	ptrint       addr;
-	u1          *pv;
-	u1          *sp;
-	functionptr  ra;
-	functionptr  xpc;
-
-	_uc = (struct ucontext *) _p;
-	_mc = &_uc->uc_mcontext;
-
-	instr = *((u4 *) (_mc->gregs[CTX_EPC]));
-	addr = _mc->gregs[(instr >> 21) & 0x1f];
-
-	if (addr == 0) {
-		pv  = (u1 *) _mc->gregs[REG_PV];
-		sp  = (u1 *) _mc->gregs[REG_SP];
-		ra  = (functionptr) _mc->gregs[REG_RA]; /* this is correct for leafs*/
-		xpc = (functionptr) _mc->gregs[CTX_EPC];
-
-		_mc->gregs[REG_ITMP1_XPTR] =
-			(ptrint) stacktrace_hardware_nullpointerexception(pv, sp, ra, xpc);
-
-		_mc->gregs[REG_ITMP2_XPC] = (ptrint) xpc;
-		_mc->gregs[CTX_EPC] = (ptrint) asm_handle_exception;
-
-	} else {
-        addr += (long) ((instr << 16) >> 16);
-
-		throw_cacao_exception_exit(string_java_lang_InternalError,
-								   "faulting address: 0x%lx at 0x%lx\n",
-								   addr, _mc->gregs[CTX_EPC]);
-	}
-}
-
-
-#if defined(USE_THREADS) && defined(NATIVE_THREADS)
-void thread_restartcriticalsection(ucontext_t *uc)
-{
-	void *critical;
-
-	critical = thread_checkcritical((void*) uc->uc_mcontext.gregs[CTX_EPC]);
-
-	if (critical)
-		uc->uc_mcontext.gregs[CTX_EPC] = (ptrint) critical;
-}
-#endif
+#include "vm/global.h"
 
 
 void docacheflush(u1 *p, long bytelen)
