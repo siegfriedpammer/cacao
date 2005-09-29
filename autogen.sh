a31d4b006ac327252b5666d63adf976b9ee9d013
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-09-29 14:13:17 +0000

    * Removed src/classpath.

--- autogen.sh
+++ autogen.sh
@@ -2,17 +2,15 @@
 
 libtoolize --automake
 if test `uname` = 'FreeBSD'; then
-    aclocal -I m4 -I src/classpath/m4 -I /usr/local/share/aclocal -I /usr/local/share/aclocal19
+    aclocal -I m4 -I /usr/local/share/aclocal -I /usr/local/share/aclocal19
 else
-    aclocal -I m4 -I src/classpath/m4
+    aclocal -I m4
 fi
 autoheader
 automake --add-missing
 autoconf
 
 cd src/boehm-gc && ./autogen.sh && cd ../..
-
-cd src/classpath && ./autogen.sh && cd ../..
 
 cd src/libltdl && ./autogen.sh && cd ../..
 
