50277ca36c91bcfa5134a04685f0f91a705fb13a
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-11-03 20:18:51 +0000

    * gnu_classpath_Pointer.h: Added, required by java_lang_VMThread.h.

---
+++ src/native/include/gnu_classpath_Pointer.h
@@ -0,0 +1,13 @@
+/* This file is machine generated, don't edit it! */
+
+#ifndef _GNU_CLASSPATH_POINTER_H
+#define _GNU_CLASSPATH_POINTER_H
+
+/* Structure information for class: gnu/classpath/Pointer */
+
+typedef struct gnu_classpath_Pointer {
+   java_objectheader header;
+} gnu_classpath_Pointer;
+
+#endif
+
