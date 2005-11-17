95f3fa420dcfc7cc9b098f05e86ddf3bdcbfc840
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-11-17 18:40:04 +0000

    * Really support build dirs.

--- tests/regression/Test.sh
+++ tests/regression/Test.sh
@@ -2,6 +2,7 @@
 
 JAVA=$1
 TEST=$2
+SRCDIR=$3
 
 echo -n "$TEST: "
 
@@ -9,38 +10,38 @@ $JAVA $TEST > $TEST.thisoutput 2>&1
 
 if [ $? -eq "0" ]; then
     # no Error returned
-    if [ -f $TEST.2output ]; then
+    if [ -f $SRCDIR/$TEST.2output ]; then
         # Error should have been returned
         echo "OK, but wrong return value: $?"
-        head $TEST.output
+        head $TEST.thisoutput
         exit
     fi
 	
-    cmp -s $TEST.output $TEST.thisoutput
+    cmp -s $SRCDIR/$TEST.output $TEST.thisoutput
 
     if [ $? -eq "0" ]; then
         echo "OK"
     else
         echo "FAILED"
-        diff -u $TEST.output $TEST.thisoutput
+        diff -u $SRCDIR/$TEST.output $TEST.thisoutput
     fi
 
 else
     # Error returned
-    if [ ! -f $TEST.2output ]; then
+    if [ ! -f $SRCDIR/$TEST.2output ]; then
         # No Error should have been returned
         echo "FAILED, but wrong return value: $?"
         head $TEST.this2output
         exit
     fi
 
-    cmp -s $TEST.2output $TEST.thisoutput
+    cmp -s $SRCDIR/$TEST.2output $TEST.thisoutput
 
     if [ $? -eq "0" ]; then
         echo "OK"
     else
         echo "FAILED"
-        diff -u $TEST.2output $TEST.thisoutput
+        diff -u $SRCDIR/$TEST.2output $TEST.thisoutput
     fi
 fi		
 
