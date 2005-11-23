e83a4188f7374cadbbba62ec5334ec59be6a8cab
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-11-23 22:55:19 +0000

    * Updated.

--- tests/prop.java
+++ tests/prop.java
@@ -1,5 +1,7 @@
+import java.io.*;
+
 public class prop {
-	public static void main(String [] s) {
-		System.getProperties().save (System.out, "alle properties:");
-		}
-	}
+    public static void main(String [] argv) throws IOException {
+        System.getProperties().store(System.out, "all properties:");
+    }
+}
