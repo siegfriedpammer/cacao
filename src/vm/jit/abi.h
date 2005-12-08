329d62d34ed87121d09daa5086f8e6854d2760ab
Christian Thalinger <twisti@complang.tuwien.ac.at>
2005-12-08 14:35:18 +0000

    * Added, contains common ABI defines.

---
+++ src/vm/jit/abi.h
@@ -0,0 +1,59 @@
+/* src/vm/jit/abi.h - common ABI defines
+
+   Copyright (C) 1996-2005 R. Grafl, A. Krall, C. Kruegel, C. Oates,
+   R. Obermaisser, M. Platter, M. Probst, S. Ring, E. Steiner,
+   C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich, J. Wenninger,
+   Institut f. Computersprachen - TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+
+   Contact: cacao@complang.tuwien.ac.at
+
+   Authors: Christian Thalinger
+
+   Changes:
+
+   $Id: abi.h 3912 2005-12-08 14:35:18Z twisti $
+
+*/
+
+
+#ifndef _ABI_H
+#define _ABI_H
+
+/* some count defines for asmpart.S *******************************************/
+
+#define SAV_CNT    (INT_SAV_CNT + FLT_SAV_CNT)
+#define ARG_CNT    (INT_ARG_CNT + FLT_ARG_CNT)
+#define TMP_CNT    (INT_TMP_CNT + FLT_TMP_CNT)
+
+#endif /* _ABI_H */
+
+
+/*
+ * These are local overrides for various environment variables in Emacs.
+ * Please do not remove this and leave it at the end of the file, where
+ * Emacs will automagically detect them.
+ * ---------------------------------------------------------------------
+ * Local variables:
+ * mode: c
+ * indent-tabs-mode: t
+ * c-basic-offset: 4
+ * tab-width: 4
+ * End:
+ */
