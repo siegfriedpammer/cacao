7075e55bb4229803f613feb9e7082145ed705bc0
Christian Thalinger <twisti@complang.tuwien.ac.at>
2006-01-01 18:21:30 +0000

    * generic_symbol_at_address: Added, they were implemented in dis-buf.c   which was removed. * generic_print_address: Likewise. * perror_memory: Likewise.

--- src/vm/jit/i386/disass.c
+++ src/vm/jit/i386/disass.c
@@ -29,16 +29,24 @@
 
    Changes: Christian Thalinger
 
-   $Id: disass.c 3177 2005-09-14 18:03:11Z twisti $
+   $Id: disass.c 4031 2006-01-01 18:21:30Z twisti $
 
 */
 
 
+#include "config.h"
+
+#include <assert.h>
+/* #include <dis-asm.h> */
 #include <stdarg.h>
 #include <string.h>
 
-#include "vm/jit/disass.h"
+#include "vm/types.h"
+
 #include "vm/jit/i386/dis-asm.h"
+
+#include "mm/memory.h"
+#include "vm/jit/disass.h"
 
 
 char mylinebuf[512];
@@ -57,6 +65,12 @@ char *regs[] = {
 };
 
 
+/* myprintf ********************************************************************
+
+   Required by i386-dis.c, prints the stuff into a buffer.
+
+*******************************************************************************/
+
 void myprintf(PTR p, const char *fmt, ...)
 {
 	va_list ap;
@@ -65,15 +79,54 @@ void myprintf(PTR p, const char *fmt, ...)
 	va_end(ap);
 }
 
+/* buffer_read_memory **********************************************************
+
+   Required by i386-dis.c, copy some stuff to another memory.
+
+*******************************************************************************/
+
 int buffer_read_memory(bfd_vma memaddr, bfd_byte *myaddr, unsigned int length, struct disassemble_info *info)
 {
-	if (length == 1)
-		*myaddr = *((u1 *) memaddr);
-	else
-		memcpy(myaddr, (void *) memaddr, length);
+	MCOPY(myaddr, (void *) memaddr, u1, length);
+
 	return 0;
 }
 
+
+/* generic_symbol_at_address ***************************************************
+
+   Required by i386-dis.c, just return 1.
+
+*******************************************************************************/
+
+int generic_symbol_at_address(bfd_vma addr, struct disassemble_info *info)
+{
+	return 1;
+}
+
+
+/* generic_print_address *******************************************************
+
+   Required by i386-dis.c, just print the address.
+
+*******************************************************************************/
+
+void generic_print_address(bfd_vma addr, struct disassemble_info *info)
+{
+	myprintf(info->stream, "0x%016lx", addr);
+}
+
+
+/* perror_memory ***************************************************************
+
+   Required by i386-dis.c, jsut assert in case.
+
+*******************************************************************************/
+
+void perror_memory(int status, bfd_vma memaddr, struct disassemble_info *info)
+{
+	assert(0);
+}
 
 
 /* disassinstr *****************************************************************
@@ -102,8 +155,8 @@ u1 *disassinstr(u1 *code)
 	mylen = 0;
 	seqlen = print_insn_i386((bfd_vma) code, &info);
 
-	for (i = 0; i < seqlen; i++) {
-		printf("%02x ", *(code + i));
+	for (i = 0; i < seqlen; i++, code++) {
+		printf("%02x ", *code);
 	}
 
 	for (; i < 8; i++) {
@@ -112,17 +165,17 @@ u1 *disassinstr(u1 *code)
 
 	printf("   %s\n", mylinebuf);
 
-	return code + seqlen;
+	return code;
 }
 
 
 
-/* function disassemble ********************************************************
+/* disassemble *****************************************************************
 
    Outputs a disassembler listing of some machine code on 'stdout'.
 
-   code: pointer to first machine instruction
-   len:  code length to display (in bytes)
+   start: pointer to first machine instruction
+   end:   pointer after last machine instruction
 
 *******************************************************************************/
 
