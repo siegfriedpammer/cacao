1c571b87aa38b7152422491e1e4a66deecffd99d
Christian Thalinger <twisti@complang.tuwien.ac.at>
2006-01-02 13:05:56 +0000

    Changed register allocator .inc to .c files and moved them to src/vm/jit/allocator

---
+++ src/vm/jit/reg.c
@@ -0,0 +1,247 @@
+/* src/vm/jit/reg.c - register allocator setup
+
+   Copyright (C) 1996-2005 R. Grafl, A. Krall, C. Kruegel, C. Oates,
+   R. Obermaisser, M. Platter, M. Probst, S. Ring, E. Steiner,
+   C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich, J. Wenninger,
+   Institut f. Computersprachen - TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+   02111-1307, USA.
+
+   Contact: cacao@complang.tuwien.ac.at
+
+   Authors: Andreas Krall
+
+   Changes: Stefan Ring
+            Christian Thalinger
+			Christian Ullrich
+            Michael Starzinger
+
+   $Id: reg.c 4056 2006-01-02 13:05:56Z christian $
+
+*/
+
+
+#include "config.h"
+#include "vm/types.h"
+
+#include "assert.h"
+
+#include "arch.h"
+#include "md-abi.h"
+
+#include "mm/memory.h"
+#include "vm/jit/reg.h"
+
+#include "md-abi.inc"
+
+/* reg_setup *******************************************************************
+
+   TODO
+
+*******************************************************************************/
+
+void reg_setup(methodinfo *m, registerdata *rd, t_inlining_globals *id)
+{
+	s4 i;
+	varinfo5 *v;
+	
+	/* setup the integer register table */
+
+#if defined(__ARM__)
+	/* On ARM longs can be split across argument regs and stack. This is
+	 * signed by setting the HIGH_REG to INT_ARG_CNT in md_param_alloc().
+	 * Here we make sure it resolves to a special dummy reg (REG_SPLIT). */
+	rd->argintregs = DMNEW(s4, INT_ARG_CNT + 1);
+	rd->argintregs[INT_ARG_CNT] = REG_SPLIT;
+#else
+	rd->argintregs = DMNEW(s4, INT_ARG_CNT);
+#endif
+	rd->tmpintregs = DMNEW(s4, INT_TMP_CNT);
+	rd->savintregs = DMNEW(s4, INT_SAV_CNT);
+	rd->freeargintregs = DMNEW(s4, INT_ARG_CNT);
+	rd->freetmpintregs = DMNEW(s4, INT_TMP_CNT);
+	rd->freesavintregs = DMNEW(s4, INT_SAV_CNT);
+
+	rd->argintreguse = 0;
+	rd->tmpintreguse = 0;
+	rd->savintreguse = 0;
+
+	for (i = 0; i < INT_REG_CNT; i++) {
+		switch (nregdescint[i]) {
+		case REG_RET:
+			rd->intreg_ret = i; 
+			break;
+		case REG_SAV:
+			rd->savintregs[rd->savintreguse++] = i;
+			break;
+		case REG_TMP:
+  			rd->tmpintregs[rd->tmpintreguse++] = i; 
+			break;
+		case REG_ARG:
+			rd->argintregs[rd->argintreguse++] = i;
+			break;
+		}
+	}
+	assert(rd->savintreguse == INT_SAV_CNT);
+	assert(rd->tmpintreguse == INT_TMP_CNT);
+	assert(rd->argintreguse == INT_ARG_CNT);
+
+#if defined(__X86_64__)
+	/* 
+	 * on x86_64 the argument registers are not in ascending order 
+	 * a00 (%rdi) <-> a03 (%rcx) and a01 (%rsi) <-> a02 (%rdx)
+	 */
+	i = rd->argintregs[3];
+	rd->argintregs[3] = rd->argintregs[0];
+	rd->argintregs[0] = i;
+
+	i = rd->argintregs[2];
+	rd->argintregs[2] = rd->argintregs[1];
+	rd->argintregs[1] = i;
+#endif
+		
+#ifdef HAS_ADDRESS_REGISTER_FILE
+	/* setup the address register table */
+
+	rd->argadrregs = DMNEW(s4, ADR_ARG_CNT);
+	rd->tmpadrregs = DMNEW(s4, ADR_TMP_CNT);
+	rd->savadrregs = DMNEW(s4, ADR_SAV_CNT);
+	rd->freeargadrregs = DMNEW(s4, ADR_ARG_CNT);
+	rd->freetmpadrregs = DMNEW(s4, ADR_TMP_CNT);
+	rd->freesavadrregs = DMNEW(s4, ADR_SAV_CNT);
+
+	rd->adrreg_argnum = 0;
+	rd->argadrreguse = 0;
+	rd->tmpadrreguse = 0;
+	rd->savadrreguse = 0;
+
+	for (i = 0; i < ADR_REG_CNT; i++) {
+		switch (nregdescadr[i]) {
+		case REG_RET:
+			rd->adrreg_ret = i; 
+			break;
+		case REG_SAV:
+			rd->savadrregs[rd->savadrreguse++] = i;
+			break;
+		case REG_TMP:
+  			rd->tmpadrregs[rd->tmpadrreguse++] = i; 
+			break;
+		case REG_ARG:
+			rd->argadrregs[rd->argadrreguse++] = i;
+			break;
+		}
+	}
+	assert(rd->savadrreguse == ADR_SAV_CNT);
+	assert(rd->tmpadrreguse == ADR_TMP_CNT);
+	assert(rd->argadrreguse == ADR_ARG_CNT);
+#endif
+		
+	/* setup the float register table */
+
+	rd->argfltregs = DMNEW(s4, FLT_ARG_CNT);
+	rd->tmpfltregs = DMNEW(s4, FLT_TMP_CNT);
+	rd->savfltregs = DMNEW(s4, FLT_SAV_CNT);
+	rd->freeargfltregs = DMNEW(s4, FLT_ARG_CNT);
+	rd->freetmpfltregs = DMNEW(s4, FLT_TMP_CNT);
+	rd->freesavfltregs = DMNEW(s4, FLT_SAV_CNT);
+
+	rd->argfltreguse = 0;
+	rd->tmpfltreguse = 0;
+	rd->savfltreguse = 0;
+
+	for (i = 0; i < FLT_REG_CNT; i++) {
+		switch (nregdescfloat[i]) {
+		case REG_RET:
+			rd->fltreg_ret = i;
+			break;
+		case REG_SAV:
+			rd->savfltregs[rd->savfltreguse++] = i;
+			break;
+		case REG_TMP:
+			rd->tmpfltregs[rd->tmpfltreguse++] = i;
+			break;
+		case REG_ARG:
+			rd->argfltregs[rd->argfltreguse++] = i;
+			break;
+		}
+	}
+	assert(rd->savfltreguse == FLT_SAV_CNT);
+	assert(rd->tmpfltreguse == FLT_TMP_CNT);
+	assert(rd->argfltreguse == FLT_ARG_CNT);
+
+
+	rd->freemem    = DMNEW(s4, id->cummaxstack);
+#if defined(HAS_4BYTE_STACKSLOT)
+	rd->freemem_2  = DMNEW(s4, id->cummaxstack);
+#endif
+	rd->locals     = DMNEW(varinfo5, id->cumlocals);
+	rd->interfaces = DMNEW(varinfo5, id->cummaxstack);
+	for (v = rd->locals, i = id->cumlocals; i > 0; v++, i--) {
+		v[0][TYPE_INT].type = -1;
+		v[0][TYPE_LNG].type = -1;
+		v[0][TYPE_FLT].type = -1;
+		v[0][TYPE_DBL].type = -1;
+		v[0][TYPE_ADR].type = -1;
+	}
+
+	for (v = rd->interfaces, i = id->cummaxstack; i > 0; v++, i--) {
+		v[0][TYPE_INT].type = -1;
+		v[0][TYPE_INT].flags = 0;
+		v[0][TYPE_LNG].type = -1;
+		v[0][TYPE_LNG].flags = 0;
+		v[0][TYPE_FLT].type = -1;
+		v[0][TYPE_FLT].flags = 0;
+		v[0][TYPE_DBL].type = -1;
+		v[0][TYPE_DBL].flags = 0;
+		v[0][TYPE_ADR].type = -1;
+		v[0][TYPE_ADR].flags = 0;
+	}
+
+#if defined(SPECIALMEMUSE)
+# if defined(__DARWIN__)
+	/* 6*4=24 byte linkage area + 8*4=32 byte minimum parameter Area */
+	rd->memuse = LA_WORD_SIZE + INT_ARG_CNT; 
+# else
+	rd->memuse = LA_WORD_SIZE;
+# endif
+#else
+	rd->memuse = 0; /* init to zero -> analyse_stack will set it to a higher  */
+	                /* value, if appropriate */
+#endif
+
+	/* Set rd->argxxxreguse to XXX_ARG_CNBT to not use unused argument        */
+	/* registers as temp registers  */
+#if defined(HAS_ADDRESS_REGISTER_FILE)
+	rd->argadrreguse = 0;
+#endif /* defined(HAS_ADDRESS_REGISTER_FILE) */
+	rd->argintreguse = 0;
+	rd->argfltreguse = 0;
+}
+
+/*
+ * These are local overrides for various environment variables in Emacs.
+ * Please do not remove this and leave it at the end of the file, where
+ * Emacs will automagically detect them.
+ * ---------------------------------------------------------------------
+ * Local variables:
+ * mode: c
+ * indent-tabs-mode: t
+ * c-basic-offset: 4
+ * tab-width: 4
+ * End:
+ */
