6e53d8b094b3a90a87c235c35bc335f1843c9584
Christian Thalinger <twisti@complang.tuwien.ac.at>
2006-01-10 22:09:07 +0000

    * ASM_CALLJAVAFUNCTION, ASM_CALLJAVAFUNCTION_ADR, ASM_CALLJAVAFUNCTION_INT,   ASM_CALLJAVAFUNCTION2, ASM_CALLJAVAFUNCTION2_ADR,   ASM_CALLJAVAFUNCTION2_INT, ASM_CALLJAVAFUNCTION2_LONG,   ASM_CALLJAVAFUNCTION2_FLOAT, ASM_CALLJAVAFUNCTION2_DOUBLE: Macros added,   these macros hide the selection between jit and intrp functions. * ASM_GETCLASSVALUES_ATOMIC: Likewise. * intrp_asm_calljavafunction, intrp_asm_calljavafunction_int: Added. * intrp_asm_calljavafunction2, intrp_asm_calljavafunction2int,   intrp_asm_calljavafunction2long, intrp_asm_calljavafunction2float,   intrp_asm_calljavafunction2double: Likewise. * intrp_asm_getclassvalues_atomic: Likewise.

--- src/vm/jit/asmpart.h
+++ src/vm/jit/asmpart.h
@@ -29,7 +29,7 @@
 
    Changes: Christian Thalinger
 
-   $Id: asmpart.h 3911 2005-12-08 14:33:46Z twisti $
+   $Id: asmpart.h 4131 2006-01-10 22:09:07Z twisti $
 
 */
 
@@ -53,6 +53,166 @@
 #include "vm/linker.h"
 #include "vm/resolve.h"
 #include "vm/jit/stacktrace.h"
+
+
+/* some macros ****************************************************************/
+
+#if defined(ENABLE_JIT)
+# if defined(ENABLE_INTRP)
+
+#  define ASM_CALLJAVAFUNCTION(m,a0,a1,a2,a3) \
+    do { \
+        if (opt_intrp) \
+            (void) intrp_asm_calljavafunction((m), (a0), (a1), (a2), (a3)); \
+        else \
+            (void) asm_calljavafunction((m), (a0), (a1), (a2), (a3)); \
+    } while (0)
+
+#  define ASM_CALLJAVAFUNCTION_ADR(o,m,a0,a1,a2,a3) \
+    do { \
+        if (opt_intrp) \
+            (o) = intrp_asm_calljavafunction((m), (a0), (a1), (a2), (a3)); \
+        else \
+            (o) = asm_calljavafunction((m), (a0), (a1), (a2), (a3)); \
+    } while (0)
+
+#  define ASM_CALLJAVAFUNCTION_INT(i,m,a0,a1,a2,a3) \
+    do { \
+        if (opt_intrp) \
+            (i) = intrp_asm_calljavafunction_int((m), (a0), (a1), (a2), (a3)); \
+        else \
+            (i) = asm_calljavafunction_int((m), (a0), (a1), (a2), (a3)); \
+    } while (0)
+
+
+#  define ASM_CALLJAVAFUNCTION2(m,count,x,callblock) \
+    do { \
+        if (opt_intrp) \
+            (void) intrp_asm_calljavafunction2((m), (count), (x), (callblock)); \
+        else \
+            (void) asm_calljavafunction2((m), (count), (x), (callblock)); \
+    } while (0)
+
+#  define ASM_CALLJAVAFUNCTION2_ADR(o,m,count,x,callblock) \
+    do { \
+        if (opt_intrp) \
+            (o) = intrp_asm_calljavafunction2((m), (count), (x), (callblock)); \
+        else \
+            (o) = asm_calljavafunction2((m), (count), (x), (callblock)); \
+    } while (0)
+
+#  define ASM_CALLJAVAFUNCTION2_INT(i,m,count,x,callblock) \
+    do { \
+        if (opt_intrp) \
+            (i) = intrp_asm_calljavafunction2int((m), (count), (x), (callblock)); \
+        else \
+            (i) = asm_calljavafunction2int((m), (count), (x), (callblock)); \
+    } while (0)
+
+#  define ASM_CALLJAVAFUNCTION2_LONG(l,m,count,x,callblock) \
+    do { \
+        if (opt_intrp) \
+            (l) = intrp_asm_calljavafunction2long((m), (count), (x), (callblock)); \
+        else \
+            (l) = asm_calljavafunction2long((m), (count), (x), (callblock)); \
+    } while (0)
+
+#  define ASM_CALLJAVAFUNCTION2_FLOAT(f,m,count,x,callblock) \
+    do { \
+        if (opt_intrp) \
+            (f) = intrp_asm_calljavafunction2float((m), (count), (x), (callblock)); \
+        else \
+            (f) = asm_calljavafunction2float((m), (count), (x), (callblock)); \
+    } while (0)
+
+#  define ASM_CALLJAVAFUNCTION2_DOUBLE(d,m,count,x,callblock) \
+    do { \
+        if (opt_intrp) \
+            (d) = intrp_asm_calljavafunction2double((m), (count), (x), (callblock)); \
+        else \
+            (d) = asm_calljavafunction2double((m), (count), (x), (callblock)); \
+    } while (0)
+
+
+#  define ASM_GETCLASSVALUES_ATOMIC(super,sub,out) \
+    do { \
+        if (opt_intrp) \
+            intrp_asm_getclassvalues_atomic((super), (sub), (out)); \
+        else \
+            asm_getclassvalues_atomic((super), (sub), (out)); \
+    } while (0)
+
+# else /* defined(ENABLE_INTRP) */
+
+#  define ASM_CALLJAVAFUNCTION(m,a0,a1,a2,a3) \
+    (void) asm_calljavafunction((m), (a0), (a1), (a2), (a3))
+
+#  define ASM_CALLJAVAFUNCTION_ADR(o,m,a0,a1,a2,a3) \
+    (o) = asm_calljavafunction((m), (a0), (a1), (a2), (a3))
+
+#  define ASM_CALLJAVAFUNCTION_INT(i,m,a0,a1,a2,a3) \
+    (i) = asm_calljavafunction_int((m), (a0), (a1), (a2), (a3))
+
+
+#  define ASM_CALLJAVAFUNCTION2(m,count,x,callblock) \
+    (void) asm_calljavafunction2((m), (count), (x), (callblock))
+
+#  define ASM_CALLJAVAFUNCTION2_ADR(o,m,count,x,callblock) \
+    (o) = asm_calljavafunction2((m), (count), (x), (callblock))
+
+#  define ASM_CALLJAVAFUNCTION2_INT(i,m,count,x,callblock) \
+    (i) = asm_calljavafunction2int((m), (count), (x), (callblock))
+
+#  define ASM_CALLJAVAFUNCTION2_LONG(l,m,count,x,callblock) \
+    (l) = asm_calljavafunction2long((m), (count), (x), (callblock))
+
+#  define ASM_CALLJAVAFUNCTION2_FLOAT(f,m,count,x,callblock) \
+    (f) = asm_calljavafunction2float((m), (count), (x), (callblock))
+
+#  define ASM_CALLJAVAFUNCTION2_DOUBLE(d,m,count,x,callblock) \
+    (d) = asm_calljavafunction2double((m), (count), (x), (callblock))
+
+
+#  define ASM_GETCLASSVALUES_ATOMIC(super,sub,out) \
+    asm_getclassvalues_atomic((super), (sub), (out))
+
+# endif /* defined(ENABLE_INTRP) */
+
+#else /* defined(ENABLE_JIT) */
+
+# define ASM_CALLJAVAFUNCTION(m,a0,a1,a2,a3) \
+    (void) intrp_asm_calljavafunction((m), (a0), (a1), (a2), (a3))
+
+# define ASM_CALLJAVAFUNCTION_ADR(o,m,a0,a1,a2,a3) \
+    (o) = intrp_asm_calljavafunction((m), (a0), (a1), (a2), (a3))
+
+# define ASM_CALLJAVAFUNCTION_INT(i,m,a0,a1,a2,a3) \
+    (i) = intrp_asm_calljavafunction_int((m), (a0), (a1), (a2), (a3))
+
+
+# define ASM_CALLJAVAFUNCTION2(m,count,x,callblock) \
+    (void) intrp_asm_calljavafunction2((m), (count), (x), (callblock))
+
+# define ASM_CALLJAVAFUNCTION2_ADR(o,m,count,x,callblock) \
+    (o) = intrp_asm_calljavafunction2((m), (count), (x), (callblock))
+
+# define ASM_CALLJAVAFUNCTION2_INT(i,m,count,x,callblock) \
+    (i) = intrp_asm_calljavafunction2int((m), (count), (x), (callblock))
+
+# define ASM_CALLJAVAFUNCTION2_LONG(l,m,count,x,callblock) \
+    (l) = intrp_asm_calljavafunction2long((m), (count), (x), (callblock))
+
+# define ASM_CALLJAVAFUNCTION2_FLOAT(f,m,count,x,callblock) \
+    (f) = intrp_asm_calljavafunction2float((m), (count), (x), (callblock))
+
+# define ASM_CALLJAVAFUNCTION2_DOUBLE(d,m,count,x,callblock) \
+    (d) = intrp_asm_calljavafunction2double((m), (count), (x), (callblock))
+
+
+#  define ASM_GETCLASSVALUES_ATOMIC(super,sub,out) \
+    intrp_asm_getclassvalues_atomic((super), (sub), (out))
+
+#endif /* defined(ENABLE_JIT) */
 
 
 typedef struct castinfo castinfo;
@@ -87,11 +247,26 @@ void asm_call_jit_compiler(void);
    with up to 4 parameters. This function calls a Java-method (which
    possibly needs compilation) with up to 4 parameters.
 */
-java_objectheader *asm_calljavafunction(methodinfo *m, void *arg1, void *arg2,
-                                        void *arg3, void *arg4);
 
-s4 asm_calljavafunction_int(methodinfo *m, void *arg1, void *arg2,
+#if defined(ENABLE_JIT)
+java_objectheader *asm_calljavafunction(methodinfo *m,
+										void *arg1, void *arg2,
+										void *arg3, void *arg4);
+
+s4 asm_calljavafunction_int(methodinfo *m,
+							void *arg1, void *arg2,
 							void *arg3, void *arg4);
+#endif
+
+#if defined(ENABLE_INTRP)
+java_objectheader *intrp_asm_calljavafunction(methodinfo *m,
+											  void *arg1, void *arg2,
+											  void *arg3, void *arg4);
+
+s4 intrp_asm_calljavafunction_int(methodinfo *m,
+								  void *arg1, void *arg2,
+								  void *arg3, void *arg4);
+#endif
 
 
 /* 
@@ -100,6 +275,8 @@ s4 asm_calljavafunction_int(methodinfo *m, void *arg1, void *arg2,
    possibly needs compilation) with up to 4 parameters. 
    also supports a return value
 */
+
+#if defined(ENABLE_JIT)
 java_objectheader *asm_calljavafunction2(methodinfo *m, u4 count, u4 size,
 										 jni_callblock *callblock);
 
@@ -114,6 +291,24 @@ float asm_calljavafunction2float(methodinfo *m, u4 count, u4 size,
 
 double asm_calljavafunction2double(methodinfo *m, u4 count, u4 size,
 								   jni_callblock *callblock);
+#endif
+
+#if defined(ENABLE_INTRP)
+java_objectheader *intrp_asm_calljavafunction2(methodinfo *m, u4 count, u4 size,
+											   jni_callblock *callblock);
+
+s4 intrp_asm_calljavafunction2int(methodinfo *m, u4 count, u4 size,
+								  jni_callblock *callblock);
+
+s8 intrp_asm_calljavafunction2long(methodinfo *m, u4 count, u4 size,
+								   jni_callblock *callblock);
+
+float intrp_asm_calljavafunction2float(methodinfo *m, u4 count, u4 size,
+									   jni_callblock *callblock);
+
+double intrp_asm_calljavafunction2double(methodinfo *m, u4 count, u4 size,
+										 jni_callblock *callblock);
+#endif
 
 
 /* We need these two labels in codegen.inc to add the asm_calljavafunction*'s
@@ -136,7 +331,15 @@ void *asm_switchstackandcall(void *stack, void *func, void **stacktopsave, void 
 extern threadcritnode asm_criticalsections;
 #endif
 
+
+#if defined(ENABLE_JIT)
 void asm_getclassvalues_atomic(vftbl_t *super, vftbl_t *sub, castinfo *out);
+#endif
+
+#if defined(ENABLE_INTRP)
+void intrp_asm_getclassvalues_atomic(vftbl_t *super, vftbl_t *sub, castinfo *out);
+#endif
+
 
 #if defined(USE_THREADS) && !defined(NATIVE_THREADS)
 void asm_perform_threadswitch(u1 **from, u1 **to, u1 **stackTop);
