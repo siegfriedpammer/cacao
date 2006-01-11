8baf3d784eabd9963a2b158cd17332a40b502d33
Christian Thalinger <twisti@complang.tuwien.ac.at>
2006-01-11 10:01:12 +0000

    * SUPPORT_FMOD: Removed. * SUPPORT_IFCVT: Replaced with SUPPORT_I2F, SUPPORT_I2D or SUPPORT_L2F,   SUPPORT_L2D. * SUPPORT_LONG_ICVT: Replaced with SUPPORT_F2L or SUPPORT_D2L. * SUPPORT_LONG_FCVT: Replaced with SUPPORT_L2F or SUPPORT_L2D.

--- src/vm/jit/mips/arch.h
+++ src/vm/jit/mips/arch.h
@@ -28,7 +28,7 @@
 
    Changes:
 
-   $Id: arch.h 3701 2005-11-17 18:57:34Z twisti $
+   $Id: arch.h 4137 2006-01-11 10:01:12Z twisti $
 
 */
 
@@ -52,9 +52,15 @@
 #define SUPPORT_FLOAT                    1
 #define SUPPORT_DOUBLE                   1
 
-#define SUPPORT_FMOD                     1
-#define SUPPORT_IFCVT                    1
-#define SUPPORT_FICVT                    0
+#define SUPPORT_I2F                      1
+#define SUPPORT_I2D                      1
+#define SUPPORT_L2F                      1
+#define SUPPORT_L2D                      1
+
+#define SUPPORT_F2I                      0
+#define SUPPORT_F2L                      0
+#define SUPPORT_D2I                      0
+#define SUPPORT_D2L                      0
 
 #define SUPPORT_LONG_ADD                 1
 #define SUPPORT_LONG_CMP                 1
@@ -63,8 +69,6 @@
 #define SUPPORT_LONG_SHIFT               1
 #define SUPPORT_LONG_MUL                 1
 #define SUPPORT_LONG_DIV                 1
-#define SUPPORT_LONG_ICVT                0
-#define SUPPORT_LONG_FCVT                1
 
 #define SUPPORT_CONST_LOGICAL            1  /* AND, OR, XOR with immediates   */
 #define SUPPORT_CONST_MUL                1  /* mutiply with immediate         */
@@ -82,9 +86,15 @@
 #define SUPPORT_FLOAT                    1
 #define SUPPORT_DOUBLE                   1
 
-#define SUPPORT_FMOD                     0
-#define SUPPORT_IFCVT                    0
-#define SUPPORT_FICVT                    0
+#define SUPPORT_I2F                      0
+#define SUPPORT_I2D                      0
+#define SUPPORT_L2F                      0
+#define SUPPORT_L2D                      0
+
+#define SUPPORT_F2I                      0
+#define SUPPORT_F2L                      0
+#define SUPPORT_D2I                      0
+#define SUPPORT_D2L                      0
 
 #define SUPPORT_LONG_ADD                 1
 #define SUPPORT_LONG_CMP                 1
@@ -93,8 +103,6 @@
 #define SUPPORT_LONG_SHIFT               0
 #define SUPPORT_LONG_MUL                 0
 #define SUPPORT_LONG_DIV                 0
-#define SUPPORT_LONG_ICVT                0
-#define SUPPORT_LONG_FCVT                0
 
 #define SUPPORT_CONST_LOGICAL            1  /* AND, OR, XOR with immediates   */
 #define SUPPORT_CONST_MUL                1  /* mutiply with immediate         */
