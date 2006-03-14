8f07915f88cfdca2a7ee1e6d259aa60e64f16b34
Edwin Steiner <edwin.steiner@gmx.net>
2006-03-14 20:57:20 +0000

    * src/vm/jit/code.h: Added GPL header and boilerplate.

--- src/vm/jit/code.h
+++ src/vm/jit/code.h
@@ -1,3 +1,38 @@
+/* vm/jit/code.h - codeinfo struct for representing compiled code
+
+   Copyright (C) 1996-2005, 2006 R. Grafl, A. Krall, C. Kruegel,
+   C. Oates, R. Obermaisser, M. Platter, M. Probst, S. Ring,
+   E. Steiner, C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich,
+   J. Wenninger, Institut f. Computersprachen - TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+
+   Contact: cacao@cacaojvm.org
+
+   Authors: Edwin Steiner
+
+   Changes:
+
+   $Id$
+
+*/
+
+
 #ifndef _CODE_H
 #define _CODE_H
 
@@ -39,8 +74,18 @@ void code_codeinfo_free(codeinfo *code);
 
 void code_free_code_of_method(methodinfo *m);
 
-#endif
+#endif /* _CODE_H */
 
-/* vim: noet ts=4 sw=4
+/*
+ * These are local overrides for various environment variables in Emacs.
+ * Please do not remove this and leave it at the end of the file, where
+ * Emacs will automagically detect them.
+ * ---------------------------------------------------------------------
+ * Local variables:
+ * mode: c
+ * indent-tabs-mode: t
+ * c-basic-offset: 4
+ * tab-width: 4
+ * End:
+ * vim:noexpandtab:sw=4:ts=4:
  */
-
