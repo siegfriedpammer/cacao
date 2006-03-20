50da75c55758244f3be83e06718e2b081304840d
Christian Thalinger <twisti@complang.tuwien.ac.at>
2006-03-20 14:43:36 +0000

    * autogen.sh, src/boehm-gc/autogen.sh: Prefix all shell variables with CACAO_ to prevent problems on OpenBSD.

--- autogen.sh
+++ autogen.sh
@@ -2,22 +2,22 @@
 
 # test for a libtoolize
 
-HAVE_LIBTOOLIZE=false
+CACAO_HAVE_LIBTOOLIZE=false
 
-for LIBTOOLIZE in libtoolize libtoolize15 glibtoolize; do
-    if ${LIBTOOLIZE} --version > /dev/null 2>&1; then
-        LIBTOOLIZE_VERSION=`${LIBTOOLIZE} --version | sed 's/^.*[^0-9.]\([0-9]\{1,\}\.[0-9.]\{1,\}\).*/\1/'`
-#        echo ${LIBTOOLIZE_VERSION}
-        case ${LIBTOOLIZE_VERSION} in
+for CACAO_LIBTOOLIZE in libtoolize libtoolize15 glibtoolize; do
+    if ${CACAO_LIBTOOLIZE} --version > /dev/null 2>&1; then
+        CACAO_LIBTOOLIZE_VERSION=`${CACAO_LIBTOOLIZE} --version | sed 's/^.*[^0-9.]\([0-9]\{1,\}\.[0-9.]\{1,\}\).*/\1/'`
+#        echo ${CACAO_LIBTOOLIZE_VERSION}
+        case ${CACAO_LIBTOOLIZE_VERSION} in
             1.5* )
-                HAVE_LIBTOOLIZE=true
+                CACAO_HAVE_LIBTOOLIZE=true
                 break;
                 ;;
         esac
     fi
 done
 
-if test ${HAVE_LIBTOOLIZE} = false; then
+if test ${CACAO_HAVE_LIBTOOLIZE} = false; then
     echo "No proper libtoolize was found."
     echo "You must have libtool 1.5 installed."
     exit 1
@@ -26,22 +26,22 @@ fi
 
 # test for a aclocal
 
-HAVE_ACLOCAL=false
+CACAO_HAVE_ACLOCAL=false
 
-for ACLOCAL in aclocal aclocal-1.9 aclocal19; do
-    if ${ACLOCAL} --version > /dev/null 2>&1; then
-        ACLOCAL_VERSION=`${ACLOCAL} --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
-#        echo ${ACLOCAL_VERSION}
-        case ${ACLOCAL_VERSION} in
+for CACAO_ACLOCAL in aclocal aclocal-1.9 aclocal19; do
+    if ${CACAO_ACLOCAL} --version > /dev/null 2>&1; then
+        CACAO_ACLOCAL_VERSION=`${CACAO_ACLOCAL} --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
+#        echo ${CACAO_ACLOCAL_VERSION}
+        case ${CACAO_ACLOCAL_VERSION} in
             1.9* )
-                HAVE_ACLOCAL=true
+                CACAO_HAVE_ACLOCAL=true
                 break;
                 ;;
         esac
     fi
 done
 
-if test ${HAVE_ACLOCAL} = false; then
+if test ${CACAO_HAVE_ACLOCAL} = false; then
     echo "No proper aclocal was found."
     echo "You must have automake 1.9 installed."
     exit 1
@@ -50,22 +50,22 @@ fi
 
 # test for a autoheader
 
-HAVE_AUTOHEADER=false
+CACAO_HAVE_AUTOHEADER=false
 
-for AUTOHEADER in autoheader autoheader259; do
-    if ${AUTOHEADER} --version > /dev/null 2>&1; then
-        AUTOHEADER_VERSION=`${AUTOHEADER} --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
-#        echo ${AUTOHEADER_VERSION}
-        case ${AUTOHEADER_VERSION} in
+for CACAO_AUTOHEADER in autoheader autoheader259; do
+    if ${CACAO_AUTOHEADER} --version > /dev/null 2>&1; then
+        CACAO_AUTOHEADER_VERSION=`${CACAO_AUTOHEADER} --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
+#        echo ${CACAO_AUTOHEADER_VERSION}
+        case ${CACAO_AUTOHEADER_VERSION} in
             2.59* )
-                HAVE_AUTOHEADER=true
+                CACAO_HAVE_AUTOHEADER=true
                 break;
                 ;;
         esac
     fi
 done
 
-if test ${HAVE_AUTOHEADER} = false; then
+if test ${CACAO_HAVE_AUTOHEADER} = false; then
     echo "No proper autoheader was found."
     echo "You must have autoconf 2.59 installed."
     exit 1
@@ -74,22 +74,22 @@ fi
 
 # test for a automake
 
-HAVE_AUTOMAKE=false
+CACAO_HAVE_AUTOMAKE=false
 
-for AUTOMAKE in automake automake-1.9 automake19; do
-    if ${AUTOMAKE} --version > /dev/null 2>&1; then
-        AUTOMAKE_VERSION=`${AUTOMAKE} --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
-#        echo ${AUTOMAKE_VERSION}
-        case ${AUTOMAKE_VERSION} in
+for CACAO_AUTOMAKE in automake automake-1.9 automake19; do
+    if ${CACAO_AUTOMAKE} --version > /dev/null 2>&1; then
+        CACAO_AUTOMAKE_VERSION=`${CACAO_AUTOMAKE} --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
+#        echo ${CACAO_AUTOMAKE_VERSION}
+        case ${CACAO_AUTOMAKE_VERSION} in
             1.9* )
-                HAVE_AUTOMAKE=true
+                CACAO_HAVE_AUTOMAKE=true
                 break;
                 ;;
         esac
     fi
 done
 
-if test ${HAVE_AUTOMAKE} = false; then
+if test ${CACAO_HAVE_AUTOMAKE} = false; then
     echo "No proper automake was found."
     echo "You must have automake 1.9 installed."
     exit 1
@@ -98,42 +98,42 @@ fi
 
 # test for a autoconf
 
-HAVE_AUTOCONF=false
+CACAO_HAVE_AUTOCONF=false
 
-for AUTOCONF in autoconf autoconf259; do
-    if ${AUTOCONF} --version > /dev/null 2>&1; then
-        AUTOCONF_VERSION=`${AUTOCONF} --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
-#        echo ${AUTOCONF_VERSION}
-        case ${AUTOCONF_VERSION} in
+for CACAO_AUTOCONF in autoconf autoconf259; do
+    if ${CACAO_AUTOCONF} --version > /dev/null 2>&1; then
+        CACAO_AUTOCONF_VERSION=`${CACAO_AUTOCONF} --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
+#        echo ${CACAO_AUTOCONF_VERSION}
+        case ${CACAO_AUTOCONF_VERSION} in
             2.59* )
-                HAVE_AUTOCONF=true
+                CACAO_HAVE_AUTOCONF=true
                 break;
                 ;;
         esac
     fi
 done
 
-if test ${HAVE_AUTOCONF} = false; then
+if test ${CACAO_HAVE_AUTOCONF} = false; then
     echo "No proper autoconf was found."
     echo "You must have autoconf 2.59 installed."
     exit 1
 fi
 
 
-${LIBTOOLIZE} --automake
+${CACAO_LIBTOOLIZE} --automake
 if test `uname` = 'FreeBSD'; then
-    ${ACLOCAL} -I m4 -I /usr/local/share/aclocal -I /usr/local/share/aclocal19
+    ${CACAO_ACLOCAL} -I m4 -I /usr/local/share/aclocal -I /usr/local/share/aclocal19
 else
-    ${ACLOCAL} -I m4
+    ${CACAO_ACLOCAL} -I m4
 fi
-${AUTOHEADER}
-${AUTOMAKE} --add-missing
-${AUTOCONF}
+${CACAO_AUTOHEADER}
+${CACAO_AUTOMAKE} --add-missing
+${CACAO_AUTOCONF}
 
-export CACAO_LIBTOOLIZE=${LIBTOOLIZE}
-export CACAO_ACLOCAL=${ACLOCAL}
-export CACAO_AUTOHEADER=${AUTOHEADER}
-export CACAO_AUTOMAKE=${AUTOMAKE}
-export CACAO_AUTOCONF=${AUTOCONF}
+export CACAO_LIBTOOLIZE
+export CACAO_ACLOCAL
+export CACAO_AUTOHEADER
+export CACAO_AUTOMAKE
+export CACAO_AUTOCONF
 
 cd src/boehm-gc && ./autogen.sh && cd ../..
--- src/boehm-gc/autogen.sh
+++ src/boehm-gc/autogen.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-LIBTOOLIZE=${CACAO_LIBTOOLIZE}
-ACLOCAL=${CACAO_ACLOCAL}
-AUTOHEADER=${CACAO_AUTOHEADER}
-AUTOMAKE=${CACAO_AUTOMAKE}
-AUTOCONF=${CACAO_AUTOCONF}
-
-${LIBTOOLIZE} --automake
+${CACAO_LIBTOOLIZE} --automake
 if test `uname` = 'FreeBSD'; then
-    ${ACLOCAL} -I . -I /usr/local/share/aclocal -I /usr/local/share/aclocal19
+    ${CACAO_ACLOCAL} -I . -I /usr/local/share/aclocal -I /usr/local/share/aclocal19
 else
-    ${ACLOCAL}
+    ${CACAO_ACLOCAL}
 fi
-${AUTOHEADER}
-${AUTOMAKE} --add-missing
-${AUTOCONF}
+${CACAO_AUTOHEADER}
+${CACAO_AUTOMAKE} --add-missing
+${CACAO_AUTOCONF}
