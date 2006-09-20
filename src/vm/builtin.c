acc6f4123517e51b5752e98171b6546f1bdb40e9
Michael Starzinger <michi@complang.tuwien.ac.at>
2006-09-20 23:22:21 +0000

    * configure.ac: Cygwin OS dir added. * src\lib\Makefile.am: Now checks for "zip.exe" as well * src\vm\jit\i386\Makefile.am: Added Cygwin OS dir. * src\vm\jit\i386\cygwin\Makefile.am: Added. * src\vm\jit\i386\cygwin\md-os.c: Added. Only contains dummy signal handlers. * src\vm\builtin.c: Undefine Bias for Cygwin because Windows uses this define. * src\vm\signal.c: No signal handlers for Cygwin.

--- src/vm/builtin.c
+++ src/vm/builtin.c
@@ -37,7 +37,7 @@
    calls instead of machine instructions, using the C calling
    convention.
 
-   $Id: builtin.c 5375 2006-09-06 16:01:23Z edwin $
+   $Id: builtin.c 5540 2006-09-20 23:22:21Z michi $
 
 */
 
@@ -57,6 +57,9 @@
 #include "md-abi.h"
 
 #include "fdlibm/fdlibm.h"
+#if defined(__CYGWIN__) && defined(Bias)
+# undef Bias
+#endif
 
 #include "mm/boehm.h"
 #include "mm/memory.h"
