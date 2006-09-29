f5b988fa50ec4811d0dcc3b6dde17fb2c56add4e
Edwin Steiner <edwin.steiner@gmx.net>
2006-09-29 14:21:42 +0000

    * src/vm/global.h (TYPE_RET): Added (backport from unified_variables).
    * src/vm/exceptions.c (exceptions_throw_verifyerror_for_stack):
    Added TYPE_RET.

--- src/vm/exceptions.c
+++ src/vm/exceptions.c
@@ -28,7 +28,7 @@
 
    Changes: Edwin Steiner
 
-   $Id: exceptions.c 5585 2006-09-29 14:09:51Z edwin $
+   $Id: exceptions.c 5586 2006-09-29 14:21:42Z edwin $
 
 */
 
@@ -1124,6 +1124,7 @@ void exceptions_throw_verifyerror_for_stack(methodinfo *m,int type)
 		case TYPE_FLT: typename = "float"; break;
 		case TYPE_DBL: typename = "double"; break;
 		case TYPE_ADR: typename = "object/array"; break;
+		case TYPE_RET: typename = "returnAddress"; break;
 		default:       typename = "<INVALID>"; assert(0); break;
 	}
 	strcat(msg, typename);
