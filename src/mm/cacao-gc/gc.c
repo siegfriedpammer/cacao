1603459883de5a8dca9087ead8ee3253eb144fdf
Michael Starzinger <michi@complang.tuwien.ac.at>
2006-11-25 16:32:52 +0000

    * src/mm/cacao-gc/gc.h: Added. * src/mm/cacao-gc/gc.c: Debugging with gc_call. Added some development breaks. Added gc_get_heap_size, gc_get_free_bytes, gc_get_total_bytes and gc_get_max_heap_size.

--- src/mm/cacao-gc/gc.c
+++ src/mm/cacao-gc/gc.c
@@ -26,8 +26,6 @@
 
    Authors: Michael Starzinger
 
-   Changes:
-
    $Id$
 
 */
@@ -38,9 +36,34 @@
 #include <stdlib.h>
 #include "vm/types.h"
 
+#if defined(ENABLE_THREADS)
+# include "threads/native/threads.h"
+#else
+# include "threads/none/threads.h"
+#endif
+
+#include "gc.h"
 #include "heap.h"
+#include "mark.h"
+#include "mm/memory.h"
 #include "toolbox/logging.h"
+#include "vm/exceptions.h"
 #include "vm/options.h"
+
+
+/* Development Break **********************************************************/
+
+#if defined(ENABLE_THREADS)
+# error "GC does not work with threads enabled!"
+#endif
+
+#if defined(ENABLE_INTRP)
+# error "GC does not work with interpreter enabled!"
+#endif
+
+#if defined(ENABLE_JVMTI)
+# error "GC does not work with JVMTI enabled!"
+#endif
 
 
 /* gc_init *********************************************************************
@@ -60,30 +83,82 @@ void gc_init(u4 heapmaxsize, u4 heapstartsize)
 	if (heap_base == NULL)
 		exceptions_throw_outofmemory_exit();
 
+	/* this is needed for linear allocation */
+	heap_ptr = heap_base;
+
 	heap_current_size = heapstartsize;
 	heap_maximal_size = heapmaxsize;
-
-	dolog("GC: Got base pointer %p", heap_base);
+	heap_free_size = heap_current_size;
+	heap_used_size = 0;
 }
 
+
+/* gc_call *********************************************************************
+
+   Forces a full collection of the whole Java Heap.
+   This is the function which is called by System.VMRuntime.gc()
+
+*******************************************************************************/
 
 void gc_call(void)
 {
+	rootset_t *rs;
+	s4         dumpsize;
+
 	if (opt_verbosegc)
-		dolog("GC: Forced Collection");
+		dolog("GC: Forced Collection ...");
+
+	/* TODO: move the following to gc_collect() */
+
+	/* remember start of dump memory area */
+	dumpsize = dump_size();
+
+	GC_LOG( heap_println_usage(); );
+	/*GC_LOG( heap_dump_region(heap_base, heap_ptr, false); );*/
+
+	/* find the rootset for the current thread */
+	rs = DNEW(rootset_t);
+	mark_rootset_from_thread(THREADOBJECT, rs);
+
+	/* mark the objects considering the given rootset */
+	mark_me(rs);
+	GC_LOG( heap_dump_region(heap_base, heap_ptr, true); );
+
+	/* compact the heap */
+	/*compact_me(rs, heap_base, heap_ptr);*/
+
+	/* TODO: check my return value! */
+	/*heap_increase_size();*/
+
+    /* free dump memory area */
+    dump_release(dumpsize);
+
+	if (opt_verbosegc)
+		dolog("GC: Forced Collection finished.");
 }
 
 
+/* Informational getter functions *********************************************/
+
+s8 gc_get_heap_size(void)     { return heap_current_size; }
+s8 gc_get_free_bytes(void)    { return heap_free_size; }
+s8 gc_get_total_bytes(void)   { return heap_used_size; }
+s8 gc_get_max_heap_size(void) { return heap_maximal_size; }
+
+
+/* Thread specific stuff ******************************************************/
+
+#if defined(ENABLE_THREADS)
 int GC_signum1()
 {
 	return SIGUSR1;
 }
 
-
 int GC_signum2()
 {
 	return SIGUSR2;
 }
+#endif
 
 
 /*
@@ -97,4 +172,5 @@ int GC_signum2()
  * c-basic-offset: 4
  * tab-width: 4
  * End:
+ * vim:noexpandtab:sw=4:ts=4:
  */
