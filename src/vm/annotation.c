9ca51a79b695ea454c3141913cbc8a3ac297fb66
Christian Thalinger <twisti@complang.tuwien.ac.at>
2006-12-16 13:04:22 +0000

    * src/vm/annotation.c (annotation_load_attribute_runtimevisibleannotations): Extended comment, removed #ifdef ENABLE_JAVASE.

--- src/vm/annotation.c
+++ src/vm/annotation.c
@@ -57,10 +57,26 @@
             element_value element;
        } element_value_pairs[num_element_value_pairs];
    }
-			   
+
+   element_value {
+       u1 tag;
+       union {
+           u2 const_value_index;
+           {
+               u2 type_name_index;
+               u2 const_name_index;
+           } enum_const_value;
+           u2 class_info_index;
+           annotation annotation_value;
+           {
+               u2 num_values;
+               element_value values[num_values];
+           } array_value;
+       } value;
+   }
+
 *******************************************************************************/
 
-#if defined(ENABLE_JAVASE)
 bool annotation_load_attribute_runtimevisibleannotations(classbuffer *cb)
 {
 	classinfo       *c;
@@ -148,7 +164,6 @@ bool annotation_load_attribute_runtimevisibleannotations(classbuffer *cb)
 
 	return true;
 }
-#endif /* defined(ENABLE_JAVASE) */
 
 
 /*
