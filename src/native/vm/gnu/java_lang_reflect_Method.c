2f0e6392e93e0115b3f19bca00600c7134f24377
Christian Thalinger <twisti@complang.tuwien.ac.at>
2007-04-16 15:29:29 +0000

    * src/native/vm/gnu/java_lang_reflect_Method.c (getSignature): Fixed string-internal warnings.

--- src/native/vm/gnu/java_lang_reflect_Method.c
+++ src/native/vm/gnu/java_lang_reflect_Method.c
@@ -22,7 +22,7 @@
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
    02110-1301, USA.
 
-   $Id: java_lang_reflect_Method.c 7573 2007-03-25 18:55:02Z twisti $
+   $Id: java_lang_reflect_Method.c 7719 2007-04-16 15:29:29Z twisti $
 
 */
 
@@ -164,9 +164,9 @@ JNIEXPORT java_lang_Object* JNICALL Java_java_lang_reflect_Method_invokeNative(J
  */
 JNIEXPORT java_lang_String* JNICALL Java_java_lang_reflect_Method_getSignature(JNIEnv *env, java_lang_reflect_Method* this)
 {
-	classinfo        *c;
-	methodinfo       *m;
-	java_lang_String *s;
+	classinfo         *c;
+	methodinfo        *m;
+	java_objectheader *o;
 
 	c = (classinfo *) this->declaringClass;
 	m = &(c->methods[this->slot]);
@@ -174,11 +174,11 @@ JNIEXPORT java_lang_String* JNICALL Java_java_lang_reflect_Method_getSignature(J
 	if (m->signature == NULL)
 		return NULL;
 
-	s = javastring_new(m->signature);
+	o = javastring_new(m->signature);
 
-	/* in error case, s == NULL */
+	/* in error case o is NULL */
 
-	return s;
+	return (java_lang_String *) o;
 }
 
 
