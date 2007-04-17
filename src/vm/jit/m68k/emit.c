e05145858d5fca3d1b0edabc5dae99a328797e71
Christian Thalinger <twisti@complang.tuwien.ac.at>
2007-04-17 22:55:21 +0000

    * src/vm/jit/m68k/emit.c: boilerplate added.

--- src/vm/jit/m68k/emit.c
+++ src/vm/jit/m68k/emit.c
@@ -684,3 +684,17 @@ void emit_exception_check_ireg(codegendata *cd, instruction *iptr)
 	}
 }
 
+
+/*
+ * These are local overrides for various environment variables in Emacs.
+ * Please do not remove this and leave it at the end of the file, where
+ * Emacs will automagically detect them.
+ * ---------------------------------------------------------------------
+ * Local variables:
+ * mode: c
+ * indent-tabs-mode: t
+ * c-basic-offset: 4
+ * tab-width: 4
+ * End:
+ * vim:noexpandtab:sw=4:ts=4:
+ */
