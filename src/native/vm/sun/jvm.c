bc5207054eea254cce3a2f4a853c560179bf35c9
Christian Thalinger <twisti@complang.tuwien.ac.at>
2007-06-28 13:40:37 +0000

    * src/native/vm/sun/jvm.c: Added copyright header.

--- src/native/vm/sun/jvm.c
+++ src/native/vm/sun/jvm.c
@@ -1,3 +1,32 @@
+/* src/native/vm/sun/jvm.c - HotSpot JVM interface functions
+
+   Copyright (C) 2007 R. Grafl, A. Krall, C. Kruegel,
+   C. Oates, R. Obermaisser, M. Platter, M. Probst, S. Ring,
+   E. Steiner, C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich,
+   J. Wenninger, Institut f. Computersprachen - TU Wien
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+
+   $Id: java_lang_Class.c 8132 2007-06-22 11:15:47Z twisti $
+
+*/
+
+
 #define PRINTJVM 0
 
 #include "config.h"
