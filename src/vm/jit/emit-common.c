05a0fd8bf51fc6fe037f11a64083ee6da528f9bb
Christian Thalinger <twisti@complang.tuwien.ac.at>
2007-08-06 16:10:42 +0000

    * src/vm/jit/emit-common.c: Set Id keyword. * src/vm/jit/emit-common.h: Likewise.

--- src/vm/jit/emit-common.c
+++ src/vm/jit/emit-common.c
@@ -22,7 +22,7 @@
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
    02110-1301, USA.
 
-   $Id: emitfuncs.c 4398 2006-01-31 23:43:08Z twisti $
+   $Id: emit-common.c 8265 2007-08-06 16:10:42Z twisti $
 
 */
 
