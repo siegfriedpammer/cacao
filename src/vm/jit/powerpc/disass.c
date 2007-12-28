fff410dca35b5ab55274d32d1d60a3a2e7129bab
Christian Thalinger <twisti@complang.tuwien.ac.at>
2007-12-28 13:50:58 +0100

    * src/vm/jit/powerpc/disass.c: Fixed copyright header. (stdint.h): Added. (vm/types.h): Removed.

--- src/vm/jit/powerpc/disass.c
+++ src/vm/jit/powerpc/disass.c
@@ -1,6 +1,6 @@
 /* src/vm/jit/powerpc/disass.c - wrapper functions for GNU binutils disassembler
 
-   Copyright (C) 1996-2005, 2006 R. Grafl, A. Krall, C. Kruegel,
+   Copyright (C) 1996-2005, 2006, 2007 R. Grafl, A. Krall, C. Kruegel,
    C. Oates, R. Obermaisser, M. Platter, M. Probst, S. Ring,
    E. Steiner, C. Thalinger, D. Thuernbeck, P. Tomsich, C. Ullrich,
    J. Wenninger, Institut f. Computersprachen - TU Wien
@@ -22,26 +22,17 @@
    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
    02110-1301, USA.
 
-   Contact: cacao@cacaojvm.org
-
-   Authors: Andreas  Krall
-            Reinhard Grafl
-
-   Changes: Stefan Ring
-            Christian Thalinger
-
-
 */
 
 
 #include "config.h"
 
 #include <dis-asm.h>
+#include <stdint.h>
 #include <stdio.h>
 
-#include "vm/types.h"
-
 #include "vm/global.h"
+
 #include "vm/jit/disass.h"
 
 
