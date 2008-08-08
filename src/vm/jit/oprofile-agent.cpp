f63ad4d9fe49849855c12042cb7b068f48549aa1
Robert Schuster <robertschuster@fsfe.org>
2008-08-08 10:43:48 +0200

    * src/vm/jit/oprofile-agent.cpp: Set source formatting to c++.

--- src/vm/jit/oprofile-agent.cpp
+++ src/vm/jit/oprofile-agent.cpp
@@ -110,7 +110,7 @@ void OprofileAgent_close() { OprofileAgent::close(); }
  * Emacs will automagically detect them.
  * ---------------------------------------------------------------------
  * Local variables:
- * mode: c
+ * mode: c++
  * indent-tabs-mode: t
  * c-basic-offset: 4
  * tab-width: 4
