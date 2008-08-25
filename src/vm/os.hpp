d75e8f84b4b1272aebc7f724554062d4d49dbce0
Christian Thalinger <twisti@complang.tuwien.ac.at>
2008-08-25 15:47:26 +0200

    * src/vm/os.hpp (os): Added readlink. (os::readlink): New function.

--- src/vm/os.hpp
+++ src/vm/os.hpp
@@ -143,6 +143,7 @@ public:
 	static inline void*  memcpy(void* dest, const void* src, size_t n);
 	static inline void*  memset(void* s, int c, size_t n);
 	static inline int    mprotect(void* addr, size_t len, int prot);
+	static inline ssize_t readlink(const char* path, char* buf, size_t bufsiz);
 	static inline int    scandir(const char* dir, struct dirent*** namelist, int(*filter)(const struct dirent*), int(*compar)(const void*, const void*));
 	static inline int    setsockopt(int s, int level, int optname, const void* optval, socklen_t optlen);
 	static inline int    shutdown(int s, int how);
@@ -476,6 +477,15 @@ inline static ssize_t system_read(int fd, void *buf, size_t count)
 	return read(fd, buf, count);
 #else
 # error read not available
+#endif
+}
+
+inline ssize_t os::readlink(const char* path, char* buf, size_t bufsiz)
+{
+#if defined(HAVE_READLINK)
+	return ::readlink(path, buf, bufsiz);
+#else
+# error readlink not available
 #endif
 }
 
