983ccce23d6bc8460aa998c8c3fd54af874adc6a
Michael Starzinger <michi@complang.tuwien.ac.at>
2008-09-25 15:00:16 +0200

    * src/threads/lockword.cpp (Lockword_init): Removed obsolete legacy C function. * src/threads/lockword.hpp: Likewise. * src/vm/javaobjects.cpp (java_lang_reflect_Constructor_create): Likewise. (java_lang_reflect_Field_create): Likewise. (java_lang_reflect_Method_create): Likewise. * src/vm/javaobjects.hpp: Likewise.

--- src/vm/javaobjects.cpp
+++ src/vm/javaobjects.cpp
@@ -120,15 +120,6 @@ java_handle_t* java_lang_reflect_Method::invoke(java_handle_t* o, java_handle_ob
 	return result;
 }
 
-
-// Legacy C interface.
-
-extern "C" {
-	java_handle_t* java_lang_reflect_Constructor_create(methodinfo* m) { return java_lang_reflect_Constructor(m).get_handle(); }
-	java_handle_t* java_lang_reflect_Field_create(fieldinfo* f) { return java_lang_reflect_Field(f).get_handle(); }
-	java_handle_t* java_lang_reflect_Method_create(methodinfo* m) { return java_lang_reflect_Method(m).get_handle(); }
-}
-
 #endif // ENABLE_JAVASE
 
 
