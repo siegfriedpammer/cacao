1bdfe568c7641a55c0fdd2c9f504b6248d46dc60
Josef Eisl <zapster@zapster.cc>
2013-02-11 19:31:28 +0100

    Changed mode to c++
    * src/vm/rt-timing.cpp: changed mode to c++
    * src/vm/rt-timing.hpp: likewise

--- src/vm/rt-timing.cpp
+++ src/vm/rt-timing.cpp
@@ -201,7 +201,7 @@ void rt_timing_print_time_stats(FILE *file)
  * Emacs will automagically detect them.
  * ---------------------------------------------------------------------
  * Local variables:
- * mode: c
+ * mode: c++
  * indent-tabs-mode: t
  * c-basic-offset: 4
  * tab-width: 4
