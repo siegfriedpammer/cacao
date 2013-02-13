066b4a8ff4bc29df3602a0c93b441fb9aecb2f2b
Josef Eisl <zapster@zapster.cc>
2013-02-13 10:50:31 +0100

    added missing include
    * src/vm/options.c: added missing include
    * src/vm/breakpoint.hpp: likewise

--- src/vm/breakpoint.hpp
+++ src/vm/breakpoint.hpp
@@ -1,6 +1,6 @@
 /* src/vm/breakpoint.hpp - breakpoint handling header
 
-   Copyright (C) 2009
+   Copyright (C) 2009-2012
    CACAOVM - Verein zur Foerderung der freien virtuellen Maschine CACAO
 
    This file is part of CACAO.
@@ -29,6 +29,7 @@
 #include "config.h"
 
 #ifdef __cplusplus
+#include <cassert>
 #include <map>
 
 
