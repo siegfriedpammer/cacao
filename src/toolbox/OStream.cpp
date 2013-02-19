7e04217031d977f7462c4a6d5ee8b9e0b66770c7
Fabian Gruber <fabian.gruber@fadeopolis.com>
2013-02-19 15:11:32 +0100

    Added new output stream class for debugging
    The class OStream provides a lightweight, std::ostream like,
    output stream implementation. It is intended for logging and
    debugging and should replace all other approaches like
    std::ostream, printf, etc.
    
    * src/toolbox/OStream.cpp: added
    * src/toolbox/OStream.hpp: likewise
    * src/toolbox/Makefile.am: added new source files

---
+++ src/toolbox/OStream.cpp
@@ -0,0 +1,390 @@
+/* src/toolbox/OStream.cpp - simple output stream
+
+   Copyright (C) 1996-2013
+   CACAOVM - Verein zur Foerderung der freien virtuellen Maschine CACAO
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+
+*/
+
+#include "toolbox/OStream.hpp"
+
+#include "threads/thread.hpp"
+
+namespace cacao {
+
+OStream& err() {
+	static OStream stream(stderr);
+
+	return stream;
+}
+
+OStream& out() {
+	static OStream stream(stdout);
+
+	return stream;
+}
+
+Left       left;
+Right      right;
+Dec        dec;
+Oct        oct;
+Hex        hex;
+FloatDec   float_dec;
+Scientific scientific;
+FloatHex   float_hex;
+Indent     indent;
+Dedent     dedent;
+Nl         nl;
+Flush      flush;
+
+ThreadId threadid;
+
+ResetColor  reset_color;
+Bold        bold;
+NoBold      nobold;
+Underline   underline;
+NoUnderline nounderline;
+
+OStream::OStream(FILE *file) : file(file), newline(true) {
+	init_flag_defaults();
+}
+
+OStream::OStream(const OStream& os) : file(os.file), newline(os.newline) {
+	init_flag_defaults();
+}
+
+void OStream::init_flag_defaults() {
+	init_transient_flags();
+	init_persistent_flags();
+}
+
+void OStream::init_transient_flags() {
+	// keep comments in Flags class in sync with this!
+
+	width = 0;
+}
+
+void OStream::init_persistent_flags() {
+	// keep comments in Flags class in sync with this!
+
+	align           = Align_left;
+	int_fmt         = OStream::IntFmt_decimal;
+	float_fmt       = OStream::FloatFmt_decimal;
+	indent_lvl      = 0;
+	prefix          = NULL;
+	prefix_color    = InvalidColor;
+}
+
+void OStream::on_newline() {
+	if (!newline) return;
+
+	newline = false;
+
+	if (prefix_color >= 0) (*this) << prefix_color;
+
+	// print thread id
+
+#if defined(ENABLE_THREADS)
+	fprintf(file, "[0x%016lx] ", (long) threads_get_current_tid());
+#endif
+
+	// print prefix
+
+	if (prefix) {
+		fputs(prefix, file);
+		fputc(' ', file);
+	}
+
+	// indent
+
+	for (size_t indent = indent_lvl * 4; indent > 0; indent--) {
+		fputc(' ', file);
+	}
+
+	if (prefix_color >= 0) (*this) << reset_color;
+}
+
+#define PRINT_INT(DEC, OCT, HEX, VAL)                                                   \
+	switch (align) {                                                                    \
+	case Align_left:                                                                    \
+		switch (int_fmt) {                                                              \
+			case IntFmt_decimal:     fprintf(file, "%-*" #DEC, (int) width, VAL);break; \
+			case IntFmt_octal:       fprintf(file, "%-*" #OCT, (int) width, VAL);break; \
+			case IntFmt_hexadecimal: fprintf(file, "%-*" #HEX, (int) width, VAL);break; \
+			default:                 assert(false && "Bad int format");                 \
+		}                                                                               \
+		break;                                                                          \
+	case Align_right:                                                                   \
+		switch (int_fmt) {                                                              \
+			case IntFmt_decimal:     fprintf(file, "%*" #DEC, (int) width, VAL);break;  \
+			case IntFmt_octal:       fprintf(file, "%*" #OCT, (int) width, VAL);break;  \
+			case IntFmt_hexadecimal: fprintf(file, "%*" #HEX, (int) width, VAL);break;  \
+			default:                 assert(false && "Bad int format");                 \
+		}                                                                               \
+		break;                                                                          \
+	default:                                                                            \
+		assert(false && "Bad alignment");                                               \
+	}
+
+#define PRINT_FLOAT(DEC, SCI, HEX, VAL)                                                   \
+	switch (align) {                                                                      \
+	case Align_left:                                                                      \
+		switch (float_fmt) {                                                              \
+			case FloatFmt_decimal:     fprintf(file, "%-*" #DEC, (int) width, VAL);break; \
+			case FloatFmt_scientific:  fprintf(file, "%-*" #SCI, (int) width, VAL);break; \
+			case FloatFmt_hexadecimal: fprintf(file, "%-*" #HEX, (int) width, VAL);break; \
+			default:                 assert(false && "Bad float format");                 \
+		}                                                                                 \
+		break;                                                                            \
+	case Align_right:                                                                     \
+		switch (float_fmt) {                                                              \
+			case FloatFmt_decimal:     fprintf(file, "%*" #DEC, (int) width, VAL);break;  \
+			case FloatFmt_scientific:  fprintf(file, "%*" #SCI, (int) width, VAL);break;  \
+			case FloatFmt_hexadecimal: fprintf(file, "%*" #HEX, (int) width, VAL);break;  \
+			default:                 assert(false && "Bad float format");                 \
+		}                                                                                 \
+		break;                                                                            \
+	default:                                                                              \
+		assert(false && "Bad alignment");                                                 \
+	}
+
+OStream& OStream::operator<<(char c) {
+	on_newline();
+
+	if (!width) {
+		fputc(c, file);
+	} else {
+		switch (align) {
+		case Align_left:
+			fprintf(file, "%-*c", (int) width, c);
+			break;
+		case Align_right:
+			fprintf(file, "%*c", (int) width, c);
+			break;
+		}
+	}
+
+	init_transient_flags();
+	return (*this);
+}
+OStream& OStream::operator<<(bool b) {
+	return (*this) << (b ? "true" : "false");
+}
+OStream& OStream::operator<<(long n) {
+	on_newline();
+
+	PRINT_INT(ld, lo, lx, n);
+
+	init_transient_flags();
+	return (*this);
+}
+OStream& OStream::operator<<(long long n) {
+	on_newline();
+
+	PRINT_INT(lld, llo, llx, n);
+
+	init_transient_flags();
+	return (*this);
+}
+OStream& OStream::operator<<(unsigned long n) {
+	on_newline();
+
+	PRINT_INT(lu, lo, lx, n);
+
+	init_transient_flags();
+	return (*this);
+}
+OStream& OStream::operator<<(unsigned long long n) {
+	on_newline();
+
+	PRINT_INT(llu, llo, llx, n);
+
+	init_transient_flags();
+	return (*this);
+}
+OStream& OStream::operator<<(double n) {
+	on_newline();
+
+	PRINT_FLOAT(f, e, a, n);
+
+	init_transient_flags();
+	return (*this);
+}
+OStream& OStream::operator<<(const void *p) {
+	on_newline();
+	OStream OS = (*this); // new OStream for new flags
+	OS << "0x" << hex << (const long int)p;
+	return (*this);
+}
+OStream& OStream::operator<<(const char *cs) {
+	on_newline();
+
+	if (!width) {
+		fputs(cs, file);
+	} else {
+		switch (align) {
+		case Align_left:
+			fprintf(file, "%-*s", (int) width, cs);
+			break;
+		case Align_right:
+			fprintf(file, "%*s", (int) width, cs);
+			break;
+		}
+	}
+
+	init_transient_flags();
+	return (*this);
+}
+
+OStream& OStream::operator<<(const SetWidth& s) {
+	width = s.width;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const SetIndent& s) {
+	indent_lvl = s.indent;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const SetPrefix& s) {
+	prefix       = s.prefix;
+	prefix_color = s.color;
+
+	return (*this);
+}
+
+OStream& OStream::operator<<(const Left&) {
+	align = OStream::Align_left;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const Right&) {
+	align = OStream::Align_right;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const Dec&) {
+	int_fmt = OStream::IntFmt_decimal;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const Oct&) {
+	int_fmt = OStream::IntFmt_octal;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const Hex&) {
+	int_fmt = OStream::IntFmt_hexadecimal;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const FloatDec&) {
+	float_fmt = OStream::FloatFmt_decimal;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const Scientific&) {
+	float_fmt = OStream::FloatFmt_scientific;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const FloatHex&) {
+	float_fmt = OStream::FloatFmt_hexadecimal;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const Indent&) {
+	indent_lvl++;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const Dedent&) {
+	indent_lvl--;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const Nl&) {
+	(*this) << '\n';
+	newline = true;
+
+	return (*this);
+}
+OStream& OStream::operator<<(const Flush&) {
+	fflush(file);
+
+	return (*this);
+}
+
+OStream& OStream::operator<<(const ThreadId&) {
+#ifdef ENABLE_THREADS
+	ptrint tid = threads_get_current_tid();
+
+	return (*this) << "[" << hex << setw(16) << tid << dec << "]";
+#else
+	return (*this) << "[thread-id]";
+#endif
+}
+
+OStream& OStream::operator<<(Color c) {
+	switch (c) {
+	case Black:       return (*this) << "\033[30m";
+	case Red:         return (*this) << "\033[31m";
+	case Green:       return (*this) << "\033[32m";
+	case Yellow:      return (*this) << "\033[33m";
+	case Blue:        return (*this) << "\033[34m";
+	case Magenta:     return (*this) << "\033[35m";
+	case Cyan:        return (*this) << "\033[36m";
+	case White:       return (*this) << "\033[37m";
+	default:
+		assert(false && "Unknown color code");
+		break;
+	}
+
+	return (*this);
+}
+OStream& OStream::operator<<(const ResetColor&) {
+	return (*this) << "\033[0m";
+}
+OStream& OStream::operator<<(const Bold&) {
+	return (*this) << "\033[1m";
+}
+OStream& OStream::operator<<(const NoBold&) {
+	return (*this) << "\033[21m";
+}
+OStream& OStream::operator<<(const Underline&) {
+	return (*this) << "\033[4m";
+}
+OStream& OStream::operator<<(const NoUnderline&) {
+	return (*this) << "\033[24m";
+}
+
+} // end namespace cacao
+
+/*
+ * These are local overrides for various environment variables in Emacs.
+ * Please do not remove this and leave it at the end of the file, where
+ * Emacs will automagically detect them.
+ * ---------------------------------------------------------------------
+ * Local variables:
+ * mode: c++
+ * indent-tabs-mode: t
+ * c-basic-offset: 4
+ * tab-width: 4
+ * End:
+ */
---
+++ src/toolbox/OStream.hpp
@@ -0,0 +1,362 @@
+/* src/toolbox/OStream.hpp - simple output stream
+
+   Copyright (C) 1996-2013
+   CACAOVM - Verein zur Foerderung der freien virtuellen Maschine CACAO
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+
+*/
+
+#ifndef OSTREAM_HPP_
+#define OSTREAM_HPP_ 1
+
+#include <cstdio>
+#include <cassert>
+
+namespace cacao {
+
+class SetWidth;
+class SetIndent;
+class SetPrefix;
+
+class Left;
+class Right;
+class Dec;
+class Oct;
+class Hex;
+class FloatDec;
+class Scientific;
+class FloatHex;
+class Indent;
+class Dedent;
+class Nl;
+class Flush;
+
+class ThreadId;
+
+// ANSI terminal control
+
+enum Color {
+	Black,
+	Red,
+	Green,
+	Yellow,
+	Blue,
+	Magenta,
+	Cyan,
+	White,
+	InvalidColor = -1
+};
+
+class ResetColor  {};
+class Bold        {};
+class NoBold      {};
+class Underline   {};
+class NoUnderline {};
+
+/// Simple stream class for formatted output
+///
+/// This class is designed for debugging, thus usability trumps performance.
+/// It mostly mimics the iostreams library, but requires no global constructors.
+/// Interally everything is forwarded to stdio
+///
+/// Every new line is prefixed with the id of the current thread and an optional
+/// prefix string.
+/// The stream does not detect if your output contains a '\n' character.
+/// You must use the manipulator #nl instead. It works like std::endl but
+/// does not flush the stream.
+///
+/// Simple examples :
+///   @code
+///		OStream os(stdout);
+///
+///		os << "Hi there, my name is " << bold << "cacao" << nobold             << nl;
+///     os << "I was born in " << 1996                                         << nl;
+///     os << "Test failures are " << underline << red << "BAD" << reset_color << nl;
+///		os                                                                     << nl;
+///	 	os << "Do you like hex? "       << hex       << 255  << dec            << nl;
+///		os << "Or floating point hex? " << float_hex << 17.3 << float_dec      << nl;
+///   @endcode
+///
+/// Unlike a std::iostream you can copy construct an OStream.
+/// A copied OStream will write to the same file but has it's own set of
+/// format flags that you can set independent of the original stream.
+/// But Colors, bold and underline are shared by all streams for a file,
+/// because they are stored in the underlying terminal.
+///	You should not write nl to the copied stream since the original will not
+/// detect the newline.
+///
+/// Example:
+///  @code
+///		struct MyLittlePony {
+///			const char *name;
+///         Color       color;
+///		};
+///
+///		OStream& operator<<(OStream& os, const MyLittlePony& mlp) {
+///			OStream os2 = os; // new stream with new flags
+///
+///			os2 << mlp.color;
+///			os2 << "My little pony is called " << setw(20) << right << mlp.name;
+///			os2 << hex;
+///
+///			// Forgot to unset hex for os2: no problem, hex flag is not shared
+///			// Forgot to unset color: big problem, colors are shared!
+///
+///			return os; // always return original stream
+///		}
+///  @endcode
+class OStream {
+public:
+	/// create a new stream with default flags
+	OStream(FILE *file);
+
+	/// copy stream
+	///
+	/// creates a new stream with the same file
+	/// but default
+	OStream(const OStream&);
+
+	OStream& operator<<(char);
+	OStream& operator<<(bool);
+	OStream& operator<<(long);
+	OStream& operator<<(unsigned long);
+	OStream& operator<<(long long );
+	OStream& operator<<(unsigned long long);
+	OStream& operator<<(double);
+	OStream& operator<<(const void*);
+	OStream& operator<<(const char*);
+
+	OStream& operator<<(unsigned int n) {
+		return this->operator<<(static_cast<unsigned long>(n));
+	}
+
+	OStream& operator<<(int n) {
+		return this->operator<<(static_cast<long>(n));
+	}
+
+	// manipulators
+	OStream& operator<<(const SetWidth&);
+	OStream& operator<<(const SetIndent&);
+	OStream& operator<<(const SetPrefix&);
+
+	OStream& operator<<(const Left&);
+	OStream& operator<<(const Right&);
+
+	OStream& operator<<(const Dec&);
+	OStream& operator<<(const Oct&);
+	OStream& operator<<(const Hex&);
+
+	OStream& operator<<(const FloatDec&);
+	OStream& operator<<(const Scientific&);
+	OStream& operator<<(const FloatHex&);
+
+	OStream& operator<<(const Indent&);
+	OStream& operator<<(const Dedent&);
+
+	OStream& operator<<(const Nl&);
+	OStream& operator<<(const Flush&);
+
+	OStream& operator<<(const ThreadId&);
+
+	// ANSI codes
+	OStream& operator<<(Color);
+
+	OStream& operator<<(const ResetColor&);
+
+	OStream& operator<<(const Bold&);
+	OStream& operator<<(const NoBold&);
+	OStream& operator<<(const Underline&);
+	OStream& operator<<(const NoUnderline&);
+private:
+	void on_newline();
+
+	/// initialize all format flags to their default value
+	void init_flag_defaults();
+
+	/// initialize all flags that only apply to one write operation
+	void init_transient_flags();
+	/// initialize all flags that survive a write operation
+	void init_persistent_flags();
+
+	/// file stream writes to
+	FILE  *file;
+
+	/// true iff we are at the beginning of a new line
+	bool newline;
+
+	enum IntegerFormat {
+		IntFmt_decimal,
+		IntFmt_octal,
+		IntFmt_hexadecimal
+	};
+	enum FloatFormat {
+		FloatFmt_decimal,
+		FloatFmt_scientific,
+		FloatFmt_hexadecimal
+	};
+
+	// ********** format flags
+
+	/// padding for next write
+	///
+	/// ! width is reset to zero by all standard write operations !
+	///
+	/// default value is 0
+	size_t width;
+
+	/// Alignment to use when padding text
+	///
+	/// default value is OStream::Align_left
+	enum {
+		Align_left,
+		Align_right
+	} align;
+
+	/// format used to print integer types
+	///
+	/// default value is \link OStream::IntegerFormat::IntFmt_decimal decimal \endlink
+	IntegerFormat int_fmt;
+
+	/// format used to print floating point types
+	///
+	/// default value is \link OStream::FloatFormat::FloatFmt_decimal decimal \endlink
+	FloatFormat float_fmt;
+
+	/// indentation level
+	///
+	/// every new line will start with OStream::Flags::indent_lvl * 4 spaces
+	///
+	/// default value is 0
+	size_t indent_lvl;
+
+	/// line prefix
+	///
+	/// ignored if NULL
+	/// will be printed at start of every new line
+	///
+	/// default value is NULL
+	const char *prefix;
+
+	/// color line prefix is printed in
+	///
+	/// ignored if negative
+	///
+	/// default value is -1
+	Color prefix_color;
+};
+
+/// Set width flag for next item to be written.
+class SetWidth {
+	size_t width;
+public:
+	inline SetWidth(size_t width) : width(width) {}
+
+friend class OStream;
+};
+
+/// Set indent level in stream
+class SetIndent {
+	size_t indent;
+public:
+	inline SetIndent(size_t indent) : indent(indent) {}
+
+friend class OStream;
+};
+
+/// Set stream prefix
+class SetPrefix {
+	const char *prefix;
+	Color       color;
+public:
+	inline SetPrefix(const char *prefix, Color color)
+	 : prefix(prefix), color(color) {}
+
+friend class OStream;
+};
+
+class Left       {};
+class Right      {};
+class Dec        {};
+class Oct        {};
+class Hex        {};
+class FloatDec   {};
+class Scientific {};
+class FloatHex   {};
+class Indent     {};
+class Dedent     {};
+class Nl         {};
+class Flush      {};
+
+class ThreadId {};
+
+class ResetColor;
+class Bold;
+class NoBold;
+class Underline;
+class NoUnderline;
+
+inline static SetWidth setw(size_t w) {
+	return SetWidth(w);
+}
+inline static SetIndent setindent(size_t i) {
+	return SetIndent(i);
+}
+inline static SetPrefix setprefix(const char *prefix, Color color) {
+	return SetPrefix(prefix, color);
+}
+
+extern Left       left;
+extern Right      right;
+extern Dec        dec;
+extern Oct        oct;
+extern Hex        hex;
+extern FloatDec   float_dec;
+extern Scientific scientific;
+extern FloatHex   float_hex;
+extern Indent     indent;
+extern Dedent     dedent;
+extern Nl         nl;
+extern Flush      flush;
+
+// pipe this into a stream to print the current thread's id
+extern ThreadId threadid;
+
+extern ResetColor  reset_color;
+extern Bold        bold;
+extern NoBold      nobold;
+extern Underline   underline;
+extern NoUnderline nounderline;
+
+} // end namespace cacao
+
+#endif // OSTREAM_HPP_
+
+
+/*
+ * These are local overrides for various environment variables in Emacs.
+ * Please do not remove this and leave it at the end of the file, where
+ * Emacs will automagically detect them.
+ * ---------------------------------------------------------------------
+ * Local variables:
+ * mode: c++
+ * indent-tabs-mode: t
+ * c-basic-offset: 4
+ * tab-width: 4
+ * End:
+ */
