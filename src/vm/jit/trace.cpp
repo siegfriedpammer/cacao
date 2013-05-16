fa67e81020e0ffb4471975fe9560f9132cd49d7a
Fabian Gruber <fabian.gruber@fadeopolis.com>
2013-05-16 08:47:44 +0200

    Increased default size of buffer for trace messages to avoid many reallocations.

--- src/vm/jit/trace.cpp
+++ src/vm/jit/trace.cpp
@@ -188,7 +188,7 @@ void trace_java_call_enter(methodinfo *m, uint64_t *arg_regs, uint64_t *stack)
 	// Create new dump memory area.
 //	DumpMemoryArea dma;
 
-	Buffer<> logtext;
+	Buffer<> logtext(128);
 
 	TRACEJAVACALLCOUNT++;
 
@@ -291,7 +291,7 @@ void trace_java_call_exit(methodinfo *m, uint64_t *return_regs)
 	// Create new dump memory area.
 //	DumpMemoryArea dma;
 
-	Buffer<> logtext;
+	Buffer<> logtext(128);
 
 	/* generate the message */
 
@@ -334,7 +334,7 @@ void trace_exception(java_object_t *xptr, methodinfo *m, void *pos)
 	// Create new dump memory area.
 //	DumpMemoryArea dma;
 
-	Buffer<> logtext;
+	Buffer<> logtext(128);
 
 	if (xptr) {
 		logtext.write("Exception ");
@@ -409,7 +409,7 @@ void trace_exception_builtin(java_handle_t* h)
 	// Create new dump memory area.
 //	DumpMemoryArea dma;
 
-	Buffer<> logtext;
+	Buffer<> logtext(128);
 
 	logtext.write("Builtin exception thrown: ");
 
