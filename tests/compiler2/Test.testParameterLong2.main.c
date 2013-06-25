c8d5f7f442b84dd6899045dc6bcc83e7a28211c6
Josef Eisl <zapster@zapster.cc>
2013-06-25 22:54:11 +0200

    compiler2: regression test added further Long tests
    * tests/compiler2/Makefile.am: likewise
    * tests/compiler2/Test.testParameterLong2.main.c: likewise
    * tests/compiler2/Test.testParameterLong2.output: likewise
    * tests/compiler2/Test.testParameterLong3.main.c: likewise
    * tests/compiler2/Test.testParameterLong3.output: likewise

---
+++ tests/compiler2/Test.testParameterLong2.main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+long testParameterLong2(long a, long b, long c, long d, long e, long f, long g, long h, long i);
+
+int main() {
+    printf("%ld\n", testParameterLong2(1,2,3,4,5,6,7,8,9));
+    return 0;
+}
---
+++ tests/compiler2/Test.testParameterLong3.main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+long testParameterLong3(long a, long b, long c, long d, long e, long f, long g, long h, long i);
+
+int main() {
+    printf("%ld\n", testParameterLong3(1,2,3,4,5,6,7,8,9));
+    return 0;
+}
