ce2e25815d8a84748d8278d3f348b256042539c7
Josef Eisl <zapster@zapster.cc>
2013-11-05 20:51:30 +0100

    future: added unordered_multimap
    * src/future/unordered_map.hpp: likewise

--- src/future/unordered_map.hpp
+++ src/future/unordered_map.hpp
@@ -123,7 +123,62 @@ public:
 
 };
 
+template<
+	class Key,
+	class T,
+	class Hash = cacao::hash<Key>,
+	class KeyEqual = std::equal_to<Key>,
+	class Allocator = std::allocator<Key>
+>
+class unordered_multimap : public _future::unordered_multimap<Key,T,Hash,KeyEqual,Allocator> {
+	typedef _future::unordered_multimap<Key,T,Hash,KeyEqual,Allocator> _Base;
+public:
+	typedef typename _Base::key_type key_type;
+	typedef typename _Base::mapped_type mapped_type;
+	typedef typename _Base::value_type value_type;
+	typedef typename _Base::size_type size_type;
+	typedef typename _Base::difference_type difference_type;
+	typedef typename _Base::hasher hasher;
+	typedef typename _Base::key_equal key_equal;
+	typedef typename _Base::allocator_type allocator_type;
+	typedef typename _Base::reference reference;
+	typedef typename _Base::const_reference const_reference;
+	typedef typename _Base::pointer pointer;
+	typedef typename _Base::const_pointer const_pointer;
+	typedef typename _Base::iterator iterator;
+	typedef typename _Base::const_iterator const_iterator;
+	typedef typename _Base::local_iterator local_iterator;
+	typedef typename _Base::const_local_iterator const_local_iterator;
+
+	explicit unordered_multimap(
+		size_type bucket_count = unordered_map_default_bucket_count,
+		const Hash& hash = Hash(),
+		const KeyEqual& equal = KeyEqual(),
+		const Allocator& alloc = Allocator())
+			: _Base(bucket_count, hash, equal, alloc) {}
+
+	explicit unordered_multimap(
+		const Allocator& alloc)
+			: _Base(alloc) {}
+
+	template<class InputIt>
+	unordered_multimap(
+		InputIt first,
+		InputIt last,
+		size_type bucket_count = unordered_map_default_bucket_count,
+		const Hash& hash = Hash(),
+		const KeyEqual& equal = KeyEqual(),
+		const Allocator& alloc = Allocator())
+			: _Base(first, last, bucket_count, hash, equal, alloc) {}
+
+	unordered_multimap(const unordered_multimap& other) : _Base(other) {}
+
+	unordered_multimap(const unordered_multimap& other, const Allocator& alloc) : _Base(other,alloc) {}
+
+};
+
 } // end namespace cacao
+
 #endif /* FUTURE_UNORDERED_MAP_HPP_ */
 
 /*
