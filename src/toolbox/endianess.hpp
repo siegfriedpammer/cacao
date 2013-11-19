13f44e479220b996048267433a0ffdfa10ac5fec
Fabian Gruber <fabian.gruber@fadeopolis.com>
2013-11-19 12:46:19 +0100

    Removed all forward declarations of functions in endianess.hpp.

--- src/toolbox/endianess.hpp
+++ src/toolbox/endianess.hpp
@@ -26,194 +26,166 @@
 #ifndef CACAO_ENDIANESS_
 #define CACAO_ENDIANESS_ 1
 
-#include <cassert>
 #include "mm/memory.hpp"
+#include "toolbox/assert.hpp"
 #include "vm/types.hpp"
 
 namespace cacao {
-	// ***** READ UNSIGNED, LITTLE ENDIAN ************************
-
-	/// read u1 from pointer little endian
-	static inline u1 read_u1_le(const u1 *src);
-
-	/// read u2 from pointer little endian
-	static inline u2 read_u2_le(const u1 *src);
-
-	/// read u4 from pointer little endian
-	static inline u4 read_u4_le(const u1 *src);
-
-	/// read u8 from pointer little endian
-	static inline u8 read_u8_le(const u1 *src);
-
-	// ***** READ SIGNED, LITTLE ENDIAN ************************
-
-	/// read s1 from pointer little endian
-	static inline s1 read_s1_le(const u1 *src) { return (s1) read_u1_le(src); }
-
-	/// read s2 from pointer little endian
-	static inline s2 read_s2_le(const u1 *src) { return (s2) read_u2_le(src); }
-
-	/// read s4 from pointer little endian
-	static inline s4 read_s4_le(const u1 *src) { return (s4) read_u4_le(src); }
-
-	/// read s8 from pointer little endian
-	static inline s8 read_s8_le(const u1 *src) { return (s8) read_u8_le(src); }
-
-	// ***** READ UNSIGNED, BIG ENDIAN ************************
-
-	/// read u1 from pointer big endian
-	static inline u1 read_u1_be(const u1 *src);
-
-	/// read u2 from pointer big endian
-	static inline u2 read_u2_be(const u1 *src);
-
-	/// read u4 from pointer big endian
-	static inline u4 read_u4_be(const u1 *src);
-
-	/// read u8 from pointer big endian
-	static inline u8 read_u8_be(const u1 *src);
-
-	// ***** READ SIGNED, BIG ENDIAN ************************
-
-	/// read s1 from pointer big endian
-	static inline s1 read_s1_be(const u1 *src) { return (s1) read_u1_be(src); }
-
-	/// read s2 from pointer big endian
-	static inline s2 read_s2_be(const u1 *src) { return (s2) read_u2_be(src); }
-
-	/// read s4 from pointer big endian
-	static inline s4 read_s4_be(const u1 *src) { return (s4) read_u4_be(src); }
-
-	/// read s8 from pointer big endian
-	static inline s8 read_s8_be(const u1 *src) { return (s8) read_u8_be(src); }
-
-	// ***** READ FLOATING POINT, BIG ENDIAN ****************
-
-	static inline float  read_float_be(const u1 *src);
-	static inline double read_double_be(const u1 *src);
-} // end namespace cacao
+	// ***** READ UNSIGNED, LITTLE ENDIAN **************************************
 
 #if defined(__I386__) || defined(__X86_64__)
 
-/* we can optimize the LE access on little endian machines without alignment */
+	// we can optimize the LE access on little endian machines without alignment
 
-static inline u1 cacao::read_u1_le(const u1 *src) { return *((u1*) src); }
-static inline u2 cacao::read_u2_le(const u1 *src) { return *((u2*) src); }
-static inline u4 cacao::read_u4_le(const u1 *src) { return *((u4*) src); }
-static inline u8 cacao::read_u8_le(const u1 *src) { return *((u8*) src); }
+	static inline uint8_t  read_u1_le(const uint8_t *src) { return *src; }
+	static inline uint16_t read_u2_le(const uint8_t *src) { return *((uint16_t*) src); }
+	static inline uint32_t read_u4_le(const uint8_t *src) { return *((uint32_t*) src); }
+	static inline uint64_t read_u8_le(const uint8_t *src) { return *((uint64_t*) src); }
 
-#else /* defined(__I386__) || defined(__X86_64__) */
+#else // defined(__I386__) || defined(__X86_64__)
 
-static inline u1 cacao::read_u1_le(const u1 *src) {
-	return *src;
-}
+	static inline uint8_t read_u1_le(const uint8_t *src) {
+		return *src;
+	}
 
-static inline u2 cacao::read_u2_le(const u1 *src) {
-	return (((u2) src[1]) << 8) +
-	       ((u2) src[0]);
-}
+	static inline uint16_t read_u2_le(const uint8_t *src) {
+		return (((uint16_t) src[1]) << 8) +
+		        ((uint16_t) src[0]);
+	}
 
-static inline u4 cacao::read_u4_le(const u1 *src) {
-	return (((u4) src[3]) << 24) +
-	       (((u4) src[2]) << 16) +
-	       (((u4) src[1]) <<  8) +
-	       ((u4) src[0]);
-}
+	static inline uint32_t read_u4_le(const uint8_t *src) {
+		return (((uint32_t) src[3]) << 24) +
+		       (((uint32_t) src[2]) << 16) +
+		       (((uint32_t) src[1]) <<  8) +
+		        ((uint32_t) src[0]);
+	}
 
-static inline u8 cacao::read_u8_le(const u1 *src) {
-	return (((u8) src[7]) << 56) +
-	       (((u8) src[6]) << 48) +
-	       (((u8) src[5]) << 40) +
-	       (((u8) src[4]) << 32) +
-	       (((u8) src[3]) << 24) +
-	       (((u8) src[2]) << 16) +
-	       (((u8) src[1]) <<  8) +
-	       ((u8) src[0]);
-}
+	static inline uint64_t read_u8_le(const uint8_t *src) {
+		return (((uint64_t) src[7]) << 56) +
+		       (((uint64_t) src[6]) << 48) +
+		       (((uint64_t) src[5]) << 40) +
+		       (((uint64_t) src[4]) << 32) +
+		       (((uint64_t) src[3]) << 24) +
+		       (((uint64_t) src[2]) << 16) +
+		       (((uint64_t) src[1]) <<  8) +
+		        ((uint64_t) src[0]);
+	}
 
-#endif /* defined(__I386__) || defined(__X86_64__) */
+#endif // defined(__I386__) || defined(__X86_64__)
 
+	// ***** READ SIGNED, LITTLE ENDIAN ****************************************
 
-/* BE macros (for Java class files ) ******************************************/
+	/// read int8_t from pointer little endian
+	static inline int8_t  read_s1_le(const uint8_t *src) { return (int8_t) read_u1_le(src); }
 
-static inline u1 cacao::read_u1_be(const u1 *src) {
-	return *src;
-}
+	/// read int16_t from pointer little endian
+	static inline int16_t read_s2_le(const uint8_t *src) { return (int16_t) read_u2_le(src); }
 
-static inline u2 cacao::read_u2_be(const u1 *src) {
-	return (((u2) src[0]) << 8) +
-	       ((u2) src[1]);
-}
+	/// read int32_t from pointer little endian
+	static inline int32_t read_s4_le(const uint8_t *src) { return (int32_t) read_u4_le(src); }
 
-static inline u4 cacao::read_u4_be(const u1 *src) {
-	return (((u4) src[0]) << 24) +
-	       (((u4) src[1]) << 16) +
-	       (((u4) src[2]) << 8) +
-	       ((u4) src[3]);
-}
+	/// read int64_t from pointer little endian
+	static inline int64_t read_s8_le(const uint8_t *src) { return (int64_t) read_u8_le(src); }
 
-static inline u8 cacao::read_u8_be(const u1 *src) {
-	return (((u8) src[0]) << 56) +
-	       (((u8) src[1]) << 48) +
-	       (((u8) src[2]) << 40) +
-	       (((u8) src[3]) << 32) +
-	       (((u8) src[4]) << 24) +
-	       (((u8) src[5]) << 16) +
-	       (((u8) src[6]) << 8) +
-	       ((u8) src[7]);
-}
+	// ***** READ UNSIGNED, BIG ENDIAN (for Java class files ) *****************
 
-static inline float cacao::read_float_be(const u1 *src) {
-	assert(sizeof(float) == 4);
-	
-	float f;
+	/// read uint8_t from pointer big endian
+	static inline uint8_t read_u1_be(const uint8_t *src) {
+		return *src;
+	}
 
-#if WORDS_BIGENDIAN == 0
-	u1 buffer[4];
-	u2 i;
+	/// read uint16_t from pointer big endian
+	static inline uint16_t read_u2_be(const uint8_t *src) {
+		return (((uint16_t) src[0]) << 8) +
+		        ((uint16_t) src[1]);
+	}
 
-	for (i = 0; i < 4; i++)
-		buffer[3 - i] = cacao::read_u1_be(src + i);
+	/// read uint32_t from pointer big endian
+	static inline uint32_t read_u4_be(const uint8_t *src) {
+		return (((uint32_t) src[0]) << 24) +
+		       (((uint32_t) src[1]) << 16) +
+		       (((uint32_t) src[2]) <<  8) +
+		        ((uint32_t) src[3]);
+	}
 
-	MCOPY((u1 *) (&f), buffer, u1, 4);
-#else
-	MCOPY((u1 *) (&f), src,    u1, 4);
-#endif
-	
-	return f;
-}
+	/// read uint64_t from pointer big endian
+	static inline uint64_t read_u8_be(const uint8_t *src) {
+		return (((uint64_t) src[0]) << 56) +
+		       (((uint64_t) src[1]) << 48) +
+		       (((uint64_t) src[2]) << 40) +
+		       (((uint64_t) src[3]) << 32) +
+		       (((uint64_t) src[4]) << 24) +
+		       (((uint64_t) src[5]) << 16) +
+		       (((uint64_t) src[6]) <<  8) +
+		        ((uint64_t) src[7]);
+	}
 
-static inline double cacao::read_double_be(const u1 *src) {
-	assert(sizeof(double) == 8);
+	// ***** READ SIGNED, BIG ENDIAN ************************
 
-	double d;
+	/// read int8_t from pointer big endian
+	static inline int8_t  read_s1_be(const uint8_t *src) { return (int8_t)  read_u1_be(src); }
 
-#if WORDS_BIGENDIAN == 0
-	u1 buffer[8];
-	u2 i; 
+	/// read int16_t from pointer big endian
+	static inline int16_t read_s2_be(const uint8_t *src) { return (int16_t) read_u2_be(src); }
 
-# if defined(__ARM__) && defined(__ARMEL__) && !defined(__VFP_FP__)
-	// On little endian ARM processors when using FPA, word order
-	// of doubles is still big endian. So take that into account
-	// here. When using VFP, word order of doubles follows byte
-	// order. (michi 2005/07/24)
+	/// read int32_t from pointer big endian
+	static inline int32_t read_s4_be(const uint8_t *src) { return (int32_t) read_u4_be(src); }
 
-	for (i = 0; i < 4; i++)
-		buffer[3 - i] = cacao::read_u1_be(src + i);
-	for (i = 0; i < 4; i++)
-		buffer[7 - i] = cacao::read_u1_be(src + i + 4);
-# else
-	for (i = 0; i < 8; i++)
-		buffer[7 - i] = cacao::read_u1_be(src + i);
-# endif
+	/// read int64_t from pointer big endian
+	static inline int64_t read_s8_be(const uint8_t *src) { return (int64_t) read_u8_be(src); }
 
-  MCOPY((u1 *) (&d), buffer, u1, 8);
-#else 
-  MCOPY((u1 *) (&d), src,    u1, 8);
-#endif
+	// ***** READ FLOATING POINT, BIG ENDIAN ****************
 
-	return d;
-}
+	static inline float read_float_be(const uint8_t *src) {
+		EXPENSIVE_ASSERT(sizeof(float) == 4);
+		
+		float f;
+
+	#if WORDS_BIGENDIAN == 0
+		uint8_t  buffer[4];
+
+		for (int i = 0; i < 4; i++)
+			buffer[3 - i] = read_u1_be(src + i);
+
+		MCOPY((uint8_t *) (&f), buffer, u1, 4);
+	#else
+		MCOPY((uint8_t *) (&f), src,    u1, 4);
+	#endif
+		
+		return f;
+	}
+
+	static inline double  read_double_be(const uint8_t *src) {
+		EXPENSIVE_ASSERT(sizeof(double) == 8);
+
+		double d;
+
+	#if WORDS_BIGENDIAN == 0
+		uint8_t buffer[8];
+
+	# if defined(__ARM__) && defined(__ARMEL__) && !defined(__VFP_FP__)
+		// On little endian ARM processors when using FPA, word order
+		// of doubles is still big endian. So take that into account
+		// here. When using VFP, word order of doubles follows byte
+		// order. (michi 2005/07/24)
+
+		for (int i = 0; i < 4; i++)
+			buffer[3 - i] = read_u1_be(src + i);
+		for (int i = 0; i < 4; i++)
+			buffer[7 - i] = read_u1_be(src + i + 4);
+	# else
+		for (int i = 0; i < 8; i++)
+			buffer[7 - i] = read_u1_be(src + i);
+	# endif
+
+	  MCOPY((uint8_t *) (&d), buffer, u1, 8);
+	#else 
+	  MCOPY((uint8_t *) (&d), src,    u1, 8);
+	#endif
+
+		return d;
+	}
+} // end namespace cacao
 
 #endif // CACAO_ENDIANESS_
 
