e89de31c865835b625e94659251dc3b6460621d9
Josef Eisl <zapster@zapster.cc>
2014-04-06 11:39:54 +0200

    Increment copyright year for -version
    * src/vm/vm.cpp: likewise

--- src/vm/vm.cpp
+++ src/vm/vm.cpp
@@ -530,7 +530,7 @@ static void version(bool opt_exit)
 	puts("java version \"" JAVA_VERSION "\"");
 	puts("CACAO version " VERSION_FULL "\n");
 
-	puts("Copyright (C) 1996-2013");
+	puts("Copyright (C) 1996-2014");
 	puts("CACAOVM - Verein zur Foerderung der freien virtuellen Maschine CACAO");
 	puts("This is free software; see the source for copying conditions.  There is NO");
 	puts("warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.");
