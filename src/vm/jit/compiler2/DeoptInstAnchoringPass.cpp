e9a675553ea7fc5f2a60a7496dcb093bf8d3b24d
MatthiasJReisinger <matthias.j.reisinger@gmail.com>
2017-01-04 11:33:13 +0100

    compiler2: Add DeoptInstAnchoringPass

---
+++ src/vm/jit/compiler2/DeoptInstAnchoringPass.cpp
@@ -0,0 +1,131 @@
+/* src/vm/jit/compiler2/DeoptInstAnchoringPass.cpp - DeoptInstAnchoringPass
+
+   Copyright (C) 2013
+   CACAOVM - Verein zur Foerderung der freien virtuellen Maschine CACAO
+
+   This file is part of CACAO.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2, or (at
+   your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+
+*/
+
+#include "vm/jit/compiler2/DeoptInstAnchoringPass.hpp"
+#include "vm/jit/compiler2/PassManager.hpp"
+#include "vm/jit/compiler2/JITData.hpp"
+#include "vm/jit/compiler2/PassUsage.hpp"
+#include "vm/jit/compiler2/MethodC2.hpp"
+#include "vm/jit/compiler2/InstructionMetaPass.hpp"
+#include "vm/jit/compiler2/DominatorPass.hpp"
+#include "vm/jit/compiler2/ScheduleEarlyPass.hpp"
+#include "vm/jit/compiler2/SSAPrinterPass.hpp"
+#include "toolbox/logging.hpp"
+
+// define name for debugging (see logging.hpp)
+#define DEBUG_NAME "compiler2/DeoptInstAnchoringPass"
+
+namespace cacao {
+namespace jit {
+namespace compiler2 {
+
+/**
+ * Finds the nearest dominator of @p begin that is the start of the branch
+ * that contains @p begin.
+ */
+BeginInst *DeoptInstAnchoringPass::find_nearest_branch_begin(BeginInst *begin) {
+	BeginInst *current_begin = begin;
+	BeginInst *idom = dominator_tree->get_idominator(current_begin);
+	while (current_begin != method->get_init_bb()
+			&& idom->get_EndInst()->succ_size() <= 1) {
+		current_begin = idom;
+		idom = dominator_tree->get_idominator(current_begin);
+	}
+	return current_begin;
+}
+
+/**
+ * Makes sure that @p deopt is anchored to a proper BeginInst.
+ */
+void DeoptInstAnchoringPass::anchor(DeoptInst *deopt) {
+	Instruction *guarded_inst = deopt->get_guarded_inst();
+	BeginInst *anchor;
+
+	if (guarded_inst->is_floating()) {
+		// Anchor the DeoptInst to the first BeginInst of the method.
+		anchor = method->get_init_bb();
+	} else {
+		// Make sure that the DeoptInst does not leave the branch of the
+		// instruction that it guards. Therefore the DeoptInst has to be
+		// anchored to the BeginInst that starts the current branch.
+		BeginInst *begin = guarded_inst->get_BeginInst();
+		assert(begin);
+		anchor = find_nearest_branch_begin(begin);
+	}
+
+	assert(anchor);
+	deopt->append_dep(anchor);
+	LOG("anchored " << deopt << " to " << anchor << nl);
+}
+
+bool DeoptInstAnchoringPass::run(JITData &JD) {
+	method = JD.get_Method();
+	dominator_tree = get_Pass<DominatorPass>();
+
+	for (Method::const_iterator i = method->begin(), e = method->end(); i != e;
+			++i) {
+		Instruction *I = *i;
+		if (I->to_DeoptInst()) {
+			DeoptInst *deopt = I->to_DeoptInst();
+			anchor(deopt);
+		}
+	}
+	return true;
+}
+
+// pass usage
+PassUsage& DeoptInstAnchoringPass::get_PassUsage(PassUsage &PU) const {
+	PU.add_requires<InstructionMetaPass>();
+	PU.add_requires<DominatorPass>();
+	PU.add_schedule_before<ScheduleEarlyPass>();
+	PU.add_schedule_before<SSAPrinterPass>();
+	return PU;
+}
+
+// the address of this variable is used to identify the pass
+char DeoptInstAnchoringPass::ID = 0;
+
+// register pass
+#if defined(ENABLE_REPLACEMENT)
+static PassRegistry<DeoptInstAnchoringPass> X("DeoptInstAnchoringPass");
+#endif
+
+} // end namespace compiler2
+} // end namespace jit
+} // end namespace cacao
+
+
+/*
+ * These are local overrides for various environment variables in Emacs.
+ * Please do not remove this and leave it at the end of the file, where
+ * Emacs will automagically detect them.
+ * ---------------------------------------------------------------------
+ * Local variables:
+ * mode: c++
+ * indent-tabs-mode: t
+ * c-basic-offset: 4
+ * tab-width: 4
+ * End:
+ * vim:noexpandtab:sw=4:ts=4:
+ */
